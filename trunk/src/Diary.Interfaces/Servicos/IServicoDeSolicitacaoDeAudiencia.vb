be8b016b0a0b934e46663f4a82186565f7c6478c
hermessilvajunior <hermessilvajunior@d652af1f-c5ef-232d-d197-2330bd19a0a3>
2010-06-29 10:00:49 +0000

    Alteraões no projeto Diary

--- trunk/src/Diary.Interfaces/Servicos/IServicoDeSolicitacaoDeAudiencia.vb
+++ trunk/src/Diary.Interfaces/Servicos/IServicoDeSolicitacaoDeAudiencia.vb
@@ -1,13 +1,15 @@
 ﻿Imports Diary.Interfaces.Negocio
+Imports Compartilhados
 
 Namespace Servicos
 
     Public Interface IServicoDeSolicitacaoDeAudiencia
+        Inherits IServico
 
         Sub Inserir(ByVal SolicitacaoDeAudiencia As ISolicitacaoDeAudiencia)
         Sub Modificar(ByVal SolicitacaoDeAudiencia As ISolicitacaoDeAudiencia)
-        Function ObtenhaSolicitacoesDeAudiencia(ByVal ApenasSolicitacoesAtivas As Boolean) As IList(Of ISolicitacaoDeAudiencia)
-        Function ObtenhaSolicitacoesDeAudienciaEntreData(ByVal DataInicial As Date, ByVal DataFinal As Date, ByVal ApenasSolicitacoesAtivas As Boolean) As IList(Of ISolicitacaoDeAudiencia)
+        Function ObtenhaSolicitacoesDeAudiencia(ByVal TrazApenasAtivas As Boolean) As IList(Of ISolicitacaoDeAudiencia)
+        Function ObtenhaSolicitacoesDeAudiencia(ByVal TrazApenasAtivas As Boolean, ByVal DataInicio As Date, ByVal DataFim As Date) As IList(Of ISolicitacaoDeAudiencia)
         Function ObtenhaSolicitacaoDeAudiencia(ByVal ID As Long) As ISolicitacaoDeAudiencia
         Sub Remover(ByVal ID As Long)
 
