f69cf5e5e2a793d6e19d1bcf86c78e5e66819772
hermessilvajunior@gmail.com <hermessilvajunior@gmail.com@d652af1f-c5ef-232d-d197-2330bd19a0a3>
2010-09-03 20:40:57 +0000

    Alterações diversas

---
+++ trunk/src/SimpleServer/Form1.vb
@@ -0,0 +1,14 @@
+﻿Imports System.Runtime.Remoting.Channels
+Imports System.Runtime.Remoting.Channels.Tcp
+Imports System.Runtime.Remoting
+Imports Compartilhados
+
+Public Class Form1
+
+    Private Sub btnIniciar_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnIniciar.Click
+        Dim tcp As TcpChannel = New TcpChannel(8080)
+
+        ChannelServices.RegisterChannel(tcp)
+        RemotingConfiguration.RegisterWellKnownServiceType(GetType(ServicoRemoto), "ServicoRemoto", WellKnownObjectMode.SingleCall)
+    End Sub
+End Class
