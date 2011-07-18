c72aa6d91260b4073945ea1db25d0e6563786c90
hermessilvajunior <hermessilvajunior@gmail.com>
2011-07-18 18:05:35 +0000

    Ajustes gerais

---
+++ src/Core.Interfaces/Mapeadores/IMapeadorDeEmpresa.vb
@@ -0,0 +1,12 @@
+﻿Imports Compartilhados.Interfaces.Core.Negocio
+
+Namespace Mapeadores
+
+    Public Interface IMapeadorDeEmpresa
+
+        Function Obtenha() As IEmpresa
+        Sub Insira(ByVal Empresa As IEmpresa)
+
+    End Interface
+
+End Namespace
