f66991fb6d0b2d48ccfe3c85c47fc0acd22630df
hermessilvajunior@gmail.com <hermessilvajunior@gmail.com@d652af1f-c5ef-232d-d197-2330bd19a0a3>
2013-08-20 21:21:33 +0000

    Adicionando implementação para permitir sqllite independente de arquitetura

--- trunk/src/Compartilhados/DBHelper/SQLiteHelper.vb
+++ trunk/src/Compartilhados/DBHelper/SQLiteHelper.vb
@@ -1,24 +1,56 @@
-﻿Imports System.Data.SQLite
-Imports System.Data.Common
+﻿Imports System.Data.Common
 Imports System.Text
+Imports System.Reflection
+Imports System.IO
 
 Namespace DBHelper
 
     Public Class SQLiteHelper
         Inherits AbstractDBHelper
 
+        Private _DllSQLLite As Assembly
+        
         Public Sub New(ByVal sStrConn As String, ByVal SistemaUtilizaSQLUpperCase As Boolean)
             MyBase.New(sStrConn, SistemaUtilizaSQLUpperCase)
+            CarregueAssemblyCorreto()
         End Sub
 
+        Private Sub CarregueAssemblyCorreto()
+
+            If Not _DllSQLLite Is Nothing Then Exit Sub
+                
+            Dim CaminhoBase = Path.Combine(Util.ObtenhaCaminhoDaPastaDoServidorDeAplicacao(), "SQLite")
+
+            If IntPtr.Size = 8 Then
+                ' 64 bits 
+                CaminhoBase = Path.Combine(CaminhoBase, "x64")
+            Else If IntPtr.Size = 4 Then 
+                '32 bits
+                CaminhoBase = Path.Combine(CaminhoBase, "x86")
+            Else
+                Throw New Exception("Não foi possível encontrar o arquivo SQLite.dll para a plataforma corrente.")
+            End If
+            _DllSQLLite = Assembly.LoadFile(Path.Combine(CaminhoBase, "System.Data.SQLite.DLL"))
+        End Sub
+
+
         Protected Overrides Function CrieConexao(ByVal StringDeConexao As String) As IDbConnection
-            Dim cConexao As IDbConnection = New SQLiteConnection(StringDeConexao)
+            Dim cConexao As IDbConnection = DirectCast(_DllSQLLite.CreateInstance("System.Data.SQLite.SQLiteConnection", True), IDbConnection)
+            cConexao.ConnectionString = StringDeConexao
             cConexao.Open()
             Return cConexao
         End Function
 
+        Private Function ObtenhaSQLiteFactory() As DbProviderFactory
+            Return DirectCast(_DllSQLLite.CreateInstance("System.Data.SQLite.SQLiteFactory", True), DbProviderFactory)
+        End Function
+
+
         Protected Overrides Function CrieDataAdapter(ByVal Comando As IDbCommand) As DbDataAdapter
-            Return New SQLiteDataAdapter(CType(Comando, SQLiteCommand))
+            Dim adaptador As DbDataAdapter = ObtenhaSQLiteFactory().CreateDataAdapter()
+
+            adaptador.SelectCommand = CType(Comando, DbCommand)
+            Return adaptador
         End Function
 
         Public Overrides Function SuporteALimite() As Boolean
