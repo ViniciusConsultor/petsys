9d1228825ee6b6bbe84e5e2d6261387c0e26523b
hermessilvajunior <hermessilvajunior@gmail.com>
2013-09-10 19:47:05 +0000

    Implementação do motor de schedule (tarefas agendadas)

---
+++ src/Core.Negocio/Schedules/ScheduleLimpaLogs.vb
@@ -0,0 +1,43 @@
+﻿Imports Compartilhados
+Imports Compartilhados.Schedule
+Imports Core.Interfaces.Negocio.Schedules
+Imports Core.Interfaces.Servicos
+Imports Compartilhados.Fabricas
+Imports System.IO
+
+Namespace Schedules
+
+    Public Class ScheduleLimpaLogs
+        Inherits Schedule
+        Implements IScheduleLimpaLogs
+
+        Protected Overrides Sub ExecuteTarefa()
+            'Quando precisar de conexao na execução da tarefa é necessário pegar nova conexao
+            'Using Servico As IServicoDeConexao = FabricaGenerica.GetInstancia.CrieObjeto(Of IServicoDeConexao)()
+            '    FabricaDeContexto.GetInstancia.GetContextoAtual.Conexao = Servico.ObtenhaConexao
+            'End Using
+
+            Dim arquivos = Directory.GetFileSystemEntries(Util.GetDiretorioLog())
+
+            For Each arquivo In arquivos
+                Dim info = New FileInfo(arquivo)
+
+                If CLng(info.LastWriteTime.ToString("yyyMMdd")) < CLng(Now.ToString("yyyMMdd")) Then
+                    File.Delete(arquivo)
+                End If
+
+            Next
+        End Sub
+
+        Protected Overrides Sub Inicialize()
+          
+        End Sub
+
+        Public Overrides ReadOnly Property Nome As String
+            Get
+                Return "Schedule limpa logs"
+            End Get
+        End Property
+    End Class
+
+End Namespace
