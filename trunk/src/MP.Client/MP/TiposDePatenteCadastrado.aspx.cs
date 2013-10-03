87f01d8aa1f17787b934ef75d01e7a5dcb0518a4
chrisleyjms@gmail.com <chrisleyjms@gmail.com@d652af1f-c5ef-232d-d197-2330bd19a0a3>
2013-10-03 02:16:33 +0000

    Solução: MP Versão: 1.0 Mensagem: Início desenvolvimento cadastro do tip de patentes.

--- trunk/src/MP.Client/MP/TiposDePatenteCadastrado.aspx.cs
+++ trunk/src/MP.Client/MP/TiposDePatenteCadastrado.aspx.cs
@@ -4,11 +4,15 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Compartilhados.Componentes.Web;
+using Compartilhados.Fabricas;
+using MP.Interfaces.Negocio;
+using MP.Interfaces.Servicos;
 using Telerik.Web.UI;
 
 namespace MP.Client.MP
 {
-    public partial class TiposDePatenteCadastrado : System.Web.UI.Page
+    public partial class TiposDePatenteCadastrado : SuperPagina
     {
         private const string ID_OBJETO = "ID_OBJETO";
         private const string CHAVE_ESTADO = "CHAVE_ESTADO";
@@ -35,7 +39,30 @@ namespace MP.Client.MP
             Session[CHAVE_ESTADO] = Estado.Inicial;
             Session[ID_OBJETO] = null;
 
-            // carregar grid Tipo de patentes Cadastradas
+            CarregueGridTipoDePatente();
+        }
+
+        private void CarregueGridTipoDePatente()
+        {
+            try
+            {
+                IList<ITipoDePatente> listaTipoDePatente = new List<ITipoDePatente>();
+
+                using (var servico = FabricaGenerica.GetInstancia().CrieObjeto<IServicoDeTipoDePatente>())
+                {
+                    listaTipoDePatente = servico.obtenhaTodosTiposDePatentes();
+                }
+
+                if(listaTipoDePatente.Count > 0)
+                {
+                    this.RadGridTipoDePatente.DataSource = listaTipoDePatente;
+                    this.RadGridTipoDePatente.DataBind();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
 
         private enum Estado : byte
@@ -47,34 +74,75 @@ namespace MP.Client.MP
             Remove
         }
 
-        
+        protected void btnNovo_Click()
+        {
+            ExibaTelaNovo();
+        }
 
+        private void ExibaTelaNovo()
+        {
+            Session[CHAVE_ESTADO] = Estado.Novo;
+
+            var URL = ObtenhaURL();
+            ScriptManager.RegisterClientScriptBlock(this, this.GetType(), new Guid().ToString(), UtilidadesWeb.ExibeJanelaModal(URL, "Cadastro de Tipos de Patentes", 650, 450), false);
+        }
+
+        private string ObtenhaURL()
+        {
+            var URL = UtilidadesWeb.ObtenhaURLCorrente();
+
+            URL = string.Concat(URL, "CadastroDeTiposDePatentes.aspx");
+            return URL;
+        }
+
+        public ITipoDePatente TipoPatenteSelecionada
+        {
+            get { return (ITipoDePatente)Session[ClientID]; }
+            set { Session.Add(this.ClientID, value); }
+        }
+
+        private void ExibaTelaModificar()
+        {
+            Session[CHAVE_ESTADO] = Estado.Modifica;
+
+            var idSelecionado = this.RadGridTipoDePatente.SelectedValue;
+
+            var URL = ObtenhaURL();
+            URL = string.Concat(URL, "?Id=", TipoPatenteSelecionada.IdTipoPatente.ToString());
+            ScriptManager.RegisterClientScriptBlock(this, GetType(), new Guid().ToString(), UtilidadesWeb.ExibeJanelaModal(URL, "Cadastro de Tipos de Patentes", 650, 450), false);
+        }
+
+        protected void btnCancela_Click()
+        {
+            ExibaTelaInicial();
+        }
+
+        private void btnModificar_Click()
+        {
+            ExibaTelaModificar();
+        }
+       
         protected void rtbToolBar_ButtonClick(object sender, RadToolBarEventArgs e)
         {
-            //switch (((RadToolBarButton)e.Item).CommandName)
-            //{
-            //    case "btnNovo":
-            //        btnNovo_Click();
-            //        break;
-            //    case "btnModificar":
-            //        btnModificar_Click();
-            //        break;
-            //    case "btnExcluir":
-            //        btnExclui_Click();
-            //        break;
-            //    case "btnSalvar":
-            //        btnSalva_Click();
-            //        break;
-            //    case "btnCancelar":
-            //        btnCancela_Click();
-            //        break;
-            //    case "btnSim":
-            //        btnSim_Click();
-            //        break;
-            //    case "btnNao":
-            //        btnNao_Click();
-            //        break;
-            //}
+            switch (((RadToolBarButton)e.Item).CommandName)
+            {
+                case "btnModificar":
+                    btnModificar_Click();
+                    break;
+                case "btnCancelar":
+                    btnCancela_Click();
+                    break;
+            }
+        }
+
+        protected override string ObtenhaIdFuncao()
+        {
+            return string.Empty;
+        }
+
+        protected override RadToolBar ObtenhaBarraDeFerramentas()
+        {
+            return rtbToolBar; 
         }
     }
 }
--- trunk/src/MP.Client/MP/TiposDePatenteCadastrado.aspx.designer.cs
+++ trunk/src/MP.Client/MP/TiposDePatenteCadastrado.aspx.designer.cs
@@ -40,12 +40,12 @@ namespace MP.Client.MP {
         protected global::System.Web.UI.WebControls.Panel PanelTpoDePatenteCadastrada;
         
         /// <summary>
-        /// RadGrid1 control.
+        /// RadGridTipoDePatente control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::Telerik.Web.UI.RadGrid RadGrid1;
+        protected global::Telerik.Web.UI.RadGrid RadGridTipoDePatente;
     }
 }
