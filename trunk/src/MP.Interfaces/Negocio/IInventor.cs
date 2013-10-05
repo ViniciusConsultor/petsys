d5402829379820ead295569306c7d34b3b26ea69
hermessilvajunior@gmail.com <hermessilvajunior@gmail.com@d652af1f-c5ef-232d-d197-2330bd19a0a3>
2013-10-05 21:07:47 +0000

    Implementação do conceito de invetor

---
+++ trunk/src/MP.Interfaces/Negocio/IInventor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Compartilhados.Interfaces.Core.Negocio;
+
+namespace MP.Interfaces.Negocio
+{
+    public interface IInventor : IPapelPessoa
+    {
+        Nullable<DateTime> DataDoCadastro { get; set; }
+        string InformacoesAdicionais { get; set; }
+    }
+}
---
+++ trunk/src/MP.Interfaces/Servicos/IServicoDeInventor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Compartilhados;
+using Compartilhados.Interfaces.Core.Negocio;
+using MP.Interfaces.Negocio;
+
+namespace MP.Interfaces.Servicos
+{
+    public interface IServicoDeInventor : IServico
+    {
+        void Inserir(IInventor inventor);
+        void Remover(long ID);
+        void Atualizar(IInventor procurador);
+        IList<IInventor> ObtenhaPorNomeComoFiltro(string nome, int quantidadeMaxima);
+        IInventor Obtenha(long ID);
+        IInventor Obtenha(IPessoa pessoa);
+    }
+}
