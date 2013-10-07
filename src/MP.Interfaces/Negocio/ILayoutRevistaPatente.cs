c3dbfaf93d5da9950c9f5ce1b0fbcc286ab54089
mrmarins89 <mrmarins89@gmail.com>
2013-10-07 06:14:03 +0000

    Implementação parcial do cadastro de de layout das revistas de patente

---
+++ src/MP.Interfaces/Negocio/ILayoutRevistaPatente.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MP.Interfaces.Negocio
+{
+    public interface ILayoutRevistaPatente
+    {
+        long Codigo { get; set; }
+        string Identificador { get; set; }
+        string DescricaoIdentificador { get; set; }
+        string DescricaoResumida { get; set; }
+        int TipoDeIdentificador { get; set; }
+        long TamanhoDoCampo { get; set; }
+        bool CampoDelimitadorDoRegistro { get; set; }
+        bool CampoIdentificadorDoProcesso { get; set; }
+        bool CampoIdentificadorDeColidencia { get; set; }
+    }
+}
---
+++ src/MP.Interfaces/Servicos/IServicoDeLayoutRevistaPatente.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MP.Interfaces.Negocio;
+
+namespace MP.Interfaces.Servicos
+{
+    public interface IServicoDeLayoutRevistaPatente
+    {
+        void Inserir(ILayoutRevistaPatente layoutRevistaPatente);
+        void Excluir(long codigo);
+        void Modificar(ILayoutRevistaPatente layoutRevistaPatente);
+    }
+}
