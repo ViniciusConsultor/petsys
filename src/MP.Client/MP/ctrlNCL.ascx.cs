79424adbebf380e0dc2625df36558850f22d77a2
hermessilvajunior <hermessilvajunior@gmail.com>
2013-10-08 00:46:50 +0000

    Adicionado user control de ncl

---
+++ src/MP.Client/MP/ctrlNCL.ascx.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Compartilhados.Componentes.Web;
+using MP.Interfaces.Negocio;
+using Telerik.Web.UI;
+
+namespace MP.Client.MP
+{
+    public partial class ctrlNCL : System.Web.UI.UserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public void Inicializa()
+        {
+            LimparControle();
+            CarregueCombo();
+        }
+
+        private void LimparControle()
+        {
+            var controle = cboNCL as Control;
+            UtilidadesWeb.LimparComponente(ref controle);
+            cboNCL.ClearSelection();
+        }
+
+
+        public string Codigo
+        {
+            get { return cboNCL.SelectedValue; }
+            set { cboNCL.SelectedValue = value; }
+        }
+
+
+        private void CarregueCombo()
+        {
+            foreach (var ncl in NCL.ObtenhaTodas())
+            {
+                var item = new RadComboBoxItem(ncl.Codigo.ToString(), ncl.Codigo.ToString());
+
+                item.Attributes.Add("Descricao", ncl.Descricao);
+                item.Attributes.Add("Natureza", ncl.Natureza.Nome);
+
+                cboNCL.Items.Add(item);
+                item.DataBind();
+            }
+        }
+    }
+}
