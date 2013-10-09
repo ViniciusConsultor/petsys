11b5ea06ed077452bbb69e21d52589ae47d4a643
chrisleyjms@gmail.com <chrisleyjms@gmail.com@d652af1f-c5ef-232d-d197-2330bd19a0a3>
2013-10-09 02:17:43 +0000

    Solução: MP Versão: 1.0 Mensagem: Correção nos cadastros.

--- trunk/src/MP.Client/MP/ctrlSituacaoDoProcesso.ascx.cs
+++ trunk/src/MP.Client/MP/ctrlSituacaoDoProcesso.ascx.cs
@@ -14,12 +14,11 @@ namespace MP.Client.MP
 {
     public partial class ctrlSituacaoDoProcesso : System.Web.UI.UserControl
     {
-        public static event SituacaoDoProcessoFoiSelecionadaEventHandler SituacaoDoProcessoFoiSelecionada;
-        public delegate void SituacaoDoProcessoFoiSelecionadaEventHandler(ISituacaoDoProcesso situacaoDoProcesso);
+        //public static event SituacaoDoProcessoFoiSelecionadaEventHandler SituacaoDoProcessoFoiSelecionada;
+        //public delegate void SituacaoDoProcessoFoiSelecionadaEventHandler(ISituacaoDoProcesso situacaoDoProcesso);
 
         protected void Page_Load(object sender, EventArgs e)
         {
-
         }
 
         public void Inicializa()
@@ -35,17 +34,7 @@ namespace MP.Client.MP
             UtilidadesWeb.LimparComponente(ref controle);
             cboSituacaoDoProcesso.ClearSelection();
         }
-
-        //public bool EnableLoadOnDemand
-        //{
-        //    set { cboSituacaoDoProcesso.EnableLoadOnDemand = value; }
-        //}
-
-        //public bool ShowDropDownOnTextboxClick
-        //{
-        //    set { cboSituacaoDoProcesso.ShowDropDownOnTextboxClick = value; }
-        //}
-
+        
         public string Codigo
         {
             get { return cboSituacaoDoProcesso.SelectedValue; }
@@ -54,27 +43,13 @@ namespace MP.Client.MP
                 var descricaoSituacao =
                         SituacaoDoProcesso.RetornaDescricaoPorCodigo(Convert.ToInt32(value));
 
+                cboSituacaoDoProcesso.Items.Clear();
+                CarregueCombo();
+
                     cboSituacaoDoProcesso.Text = descricaoSituacao;
                     cboSituacaoDoProcesso.SelectedValue = value;
             }
         }
-
-        //public string DescricaoSituacao
-        //{
-        //    get { return cboSituacaoDoProcesso.Attributes["DescricaoSituacao"]; }
-        //    set { cboSituacaoDoProcesso.Attributes["DescricaoSituacao"] = value; }
-        //}
-
-        //public ISituacaoDoProcesso SituacaoDoProcessoSelecionada
-        //{
-        //    get { return (ISituacaoDoProcesso)ViewState[ClientID]; }
-        //    set { ViewState.Add(this.ClientID, value); }
-        //}
-
-        //public string TextoItemVazio
-        //{
-        //    set { cboSituacaoDoProcesso.EmptyMessage = value; }
-        //}
 
         private void CarregueCombo()
         {
@@ -91,46 +66,12 @@ namespace MP.Client.MP
 
         protected void cboSituacaoDoProcesso_ItemsRequested(object sender, RadComboBoxItemsRequestedEventArgs e)
         {
-            //IList<ISituacaoDoProcesso> listaSituacaoDoProcesso = new List<ISituacaoDoProcesso>();
-
-            //using (var servico = FabricaGenerica.GetInstancia().CrieObjeto<IServicoDeSituacaoDoProcesso>())
-            //{
-            //    listaSituacaoDoProcesso = servico.obtenhaTodasSituacoesDoProcesso();
-            //}
-
-            //if (listaSituacaoDoProcesso.Count > 0)
-            //{
-            //    foreach (var situacaoDoProcesso in listaSituacaoDoProcesso)
-            //    {
-            //        var item = new RadComboBoxItem(situacaoDoProcesso.IdSituacaoProcesso.Value.ToString(), situacaoDoProcesso.IdSituacaoProcesso.Value.ToString());
-
-            //        item.Attributes.Add("DescricaoSituacao",
-            //                            situacaoDoProcesso.DescricaoSituacao ?? "Não informada");
-
-            //        this.cboSituacaoDoProcesso.Items.Add(item);
-            //        item.DataBind();
-            //    }
-            //}
+            
         }
 
         protected void cboSituacaoDoProcesso_SelectedIndexChanged(object o, RadComboBoxSelectedIndexChangedEventArgs e)
         {
-            //if (string.IsNullOrEmpty(((RadComboBox)o).SelectedValue))
-            //    return;
-
-            //string descricao = SituacaoDoProcesso.RetornaDescricaoPorCodigo(Convert.ToInt32(((RadComboBox)o).SelectedValue));
-
-            //cboSituacaoDoProcesso.Text = descricao;
+            
         }
-
-        //public bool AutoPostBack
-        //{
-        //    set { cboSituacaoDoProcesso.AutoPostBack = value; }
-        //}
-
-        //public ctrlSituacaoDoProcesso()
-        //{
-        //    Load += Page_Load;
-        //}
     }
 }
