a3fc3eea5142f7e72857dd8cd5b58e4d23ccdfe4
chrisleyjms@gmail.com <chrisleyjms@gmail.com@d652af1f-c5ef-232d-d197-2330bd19a0a3>
2013-10-10 02:13:32 +0000

    Solução: MP Versão: 1.0 Mensagem: Inicio desenvolvimento do cadastro de marcas

---
+++ trunk/src/MP.Interfaces/Mapeadores/IMapeadorDeMarcas.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MP.Interfaces.Negocio;
+
+namespace MP.Interfaces.Mapeadores
+{
+    public interface IMapeadorDeMarcas
+    {
+        IList<IMarcas> obtenhaTodasMarcasCadastradas();
+        IMarcas obtenhaMarcasPeloId(long idMarca);
+        IList<IMarcas> obtenhaMarcasPelaDescricao(string descricaoDaMarca);
+        IList<IMarcas> ObtenhaPorIdDaMarcaComoFiltro(string idMarca, int quantidadeMaximaDeRegistros);
+        void Inserir(IMarcas marca);
+        void Modificar(IMarcas marca);
+        void Excluir(long idMarca);
+    }
+}
---
+++ trunk/src/MP.Negocio/Marcas.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MP.Interfaces.Negocio;
+
+namespace MP.Negocio
+{
+    [Serializable]
+    public class Marcas : IMarcas
+    {
+        public long? IdMarca
+        { get; set; }
+
+        public int CodigoNCL
+        { get; set; }
+
+        public int CodigoApresentacao
+        { get; set; }
+
+        public int CodigoNatureza
+        { get; set; }
+
+        public string DescricaoDaMarca
+        { get; set; }
+
+        public string EspecificacaoDeProdutosEServicos
+        { get; set; }
+
+        public string ImagemDaMarca
+        { get; set; }
+
+        public string ObservacaoDaMarca
+        { get; set; }
+
+        public int CodigoDaClasse
+        { get; set; }
+
+        public int CodigoDaSubClasse1
+        { get; set; }
+
+        public int CodigoDaSubClasse2
+        { get; set; }
+
+        public int CodigoDaSubClasse3
+        { get; set; }
+    }
+}
