ee4e7af869550da72d854f574ad5b5440dbf17a1
chrisleyjms <chrisleyjms@gmail.com>
2013-10-10 02:13:32 +0000

    Solução: MP Versão: 1.0 Mensagem: Inicio desenvolvimento do cadastro de marcas

---
+++ src/MP.Interfaces/Negocio/IMarcas.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MP.Interfaces.Negocio
+{
+    public interface IMarcas
+    {
+        long? IdMarca { get; set; }
+        int CodigoNCL { get; set; }
+        int CodigoApresentacao { get; set; }
+        //Cliente Cliente { get; set; }
+        int CodigoNatureza { get; set; }
+        string DescricaoDaMarca { get; set; }
+        string EspecificacaoDeProdutosEServicos { get; set; }
+        string ImagemDaMarca { get; set; }
+        string ObservacaoDaMarca { get; set; }
+        int CodigoDaClasse { get; set; }
+        int CodigoDaSubClasse1 { get; set; }
+        int CodigoDaSubClasse2 { get; set; }
+        int CodigoDaSubClasse3 { get; set; }
+    }
+}
---
+++ src/MP.Interfaces/Servicos/IServicoDeMarcas.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Compartilhados;
+using MP.Interfaces.Negocio;
+
+namespace MP.Interfaces.Servicos
+{
+    public interface IServicoDeMarcas : IServico
+    {
+        IList<IMarcas> obtenhaTodasMarcasCadastradas();
+        IMarcas obtenhaMarcasPeloId(long idMarca);
+        IList<IMarcas> obtenhaMarcasPelaDescricao(string descricaoDaMarca);
+        IList<IMarcas> ObtenhaPorIdDaMarcaComoFiltro(string idMarca, int quantidadeMaximaDeRegistros);
+        void Inserir(IMarcas marca);
+        void Modificar(IMarcas marca);
+        void Excluir(long idMarca);
+    }
+}
