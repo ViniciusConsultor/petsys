ce44505090f0173318a19362b6d744b642cc0965
chrisleyjms <chrisleyjms@gmail.com>
2013-10-11 17:11:06 +0000

    Solução: MP Versão: 1.0 Msg: Desenvolvimento do cadastro de marcas

---
+++ src/MP.Client/MP/ctrlMarcas.ascx
@@ -0,0 +1,57 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="ctrlMarcas.ascx.cs" Inherits="MP.Client.MP.ctrlMarcas" %>
+<%@ Register Assembly="Telerik.Web.UI" Namespace="Telerik.Web.UI" TagPrefix="telerik" %>
+<asp:Panel ID="pnlMarcas" runat="server" Width="100%">
+<table class="tabela">
+        <tr>
+            <td class="th3">
+                <asp:Label ID="Label6" runat="server" Text="Marcas cadastradas"></asp:Label>
+            </td>
+            <td class="td">
+                <telerik:RadComboBox ID="cboMarcas" runat="server" EmptyMessage="Selecione uma marca..."
+                    EnableLoadOnDemand="True" LoadingMessage="Carregando..." MarkFirstMatch="false"
+                    ShowDropDownOnTextboxClick="False" AllowCustomText="True" HighlightTemplatedItems="True"
+                    Width="90%" Skin="Vista" CausesValidation="False" AutoPostBack="True" 
+                    onitemsrequested="cboMarca_ItemsRequested" 
+                    onselectedindexchanged="cboMarca_SelectedIndexChanged">
+                    <HeaderTemplate>
+                        <table width="96%">
+                            <tr>
+                                <td width="60%">
+                                    Marca
+                                </td>
+                                <td width="13%">
+                                    Apresentação
+                                </td>
+                                 <td width="13%">
+                                    Natureza
+                                </td>  
+                                 <td width="10%">
+                                    NCL
+                                </td>                                
+                            </tr>
+                        </table>
+                    </HeaderTemplate>
+                    <ItemTemplate>
+                        <table width="100%">
+                            <tr>
+                                <td width="60%">
+                                    <%# DataBinder.Eval(Container, "Text")%>
+                                </td>
+                                <td width="13%">
+                                    <%#DataBinder.Eval(Container, "Attributes['Apresentacao']")%>
+                                </td>
+                                <td width="13%">
+                                    <%#DataBinder.Eval(Container, "Attributes['Natureza']")%>
+                                </td>      
+                                <td width="10%">
+                                    <%#DataBinder.Eval(Container, "Attributes['NCL']")%>
+                                </td>                                
+                            </tr>
+                        </table>
+                    </ItemTemplate>
+                </telerik:RadComboBox>                
+               
+            </td>
+        </tr>
+    </table>
+</asp:Panel>
---
+++ src/MP.Servicos.Local/ServicoDeMarcasLocal.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Compartilhados;
+using Compartilhados.Fabricas;
+using MP.Interfaces.Mapeadores;
+using MP.Interfaces.Negocio;
+using MP.Interfaces.Servicos;
+
+namespace MP.Servicos.Local
+{
+    public class ServicoDeMarcasLocal : Servico, IServicoDeMarcas
+    {
+        public ServicoDeMarcasLocal(ICredencial Credencial) : base(Credencial)
+        {
+        }
+
+        public IList<IMarcas> obtenhaTodasMarcasCadastradas()
+        {
+            ServerUtils.setCredencial(_Credencial);
+
+            var mapeador = FabricaGenerica.GetInstancia().CrieObjeto<IMapeadorDeMarcas>();
+
+            try
+            {
+                return mapeador.obtenhaTodasMarcasCadastradas();
+            }
+            finally
+            {
+                ServerUtils.libereRecursos();
+            }
+        }
+
+        public IMarcas obtenhaMarcasPeloId(long idMarca)
+        {
+            ServerUtils.setCredencial(_Credencial);
+
+            var mapeador = FabricaGenerica.GetInstancia().CrieObjeto<IMapeadorDeMarcas>();
+
+            try
+            {
+                return mapeador.obtenhaMarcasPeloId(idMarca);
+            }
+            finally
+            {
+                ServerUtils.libereRecursos();
+            }
+        }
+
+        public IList<IMarcas> obtenhaMarcasPelaDescricao(string descricaoDaMarca)
+        {
+            ServerUtils.setCredencial(_Credencial);
+
+            var mapeador = FabricaGenerica.GetInstancia().CrieObjeto<IMapeadorDeMarcas>();
+
+            try
+            {
+                return mapeador.obtenhaMarcasPelaDescricao(descricaoDaMarca);
+            }
+            finally
+            {
+                ServerUtils.libereRecursos();
+            }
+        }
+
+        public IList<IMarcas> ObtenhaPorIdDaMarcaComoFiltro(string idMarca, int quantidadeMaximaDeRegistros)
+        {
+            ServerUtils.setCredencial(_Credencial);
+
+            var mapeador = FabricaGenerica.GetInstancia().CrieObjeto<IMapeadorDeMarcas>();
+
+            try
+            {
+                return mapeador.ObtenhaPorIdDaMarcaComoFiltro(idMarca, quantidadeMaximaDeRegistros);
+            }
+            finally
+            {
+                ServerUtils.libereRecursos();
+            }
+        }
+
+        public void Inserir(IMarcas marca)
+        {
+            ServerUtils.setCredencial(_Credencial);
+
+            var mapeador = FabricaGenerica.GetInstancia().CrieObjeto<IMapeadorDeMarcas>();
+
+            try
+            {
+                ServerUtils.BeginTransaction();
+                mapeador.Inserir(marca);
+                ServerUtils.CommitTransaction();
+            }
+            catch
+            {
+                ServerUtils.RollbackTransaction();
+                throw;
+            }
+            finally
+            {
+                ServerUtils.libereRecursos();
+            }
+        }
+
+        public void Modificar(IMarcas marca)
+        {
+            ServerUtils.setCredencial(_Credencial);
+
+            var mapeador = FabricaGenerica.GetInstancia().CrieObjeto<IMapeadorDeMarcas>();
+
+            try
+            {
+                ServerUtils.BeginTransaction();
+                mapeador.Modificar(marca);
+                ServerUtils.CommitTransaction();
+            }
+            catch
+            {
+                ServerUtils.RollbackTransaction();
+                throw;
+            }
+            finally
+            {
+                ServerUtils.libereRecursos();
+            }
+        }
+
+        public void Excluir(long idMarca)
+        {
+            ServerUtils.setCredencial(_Credencial);
+
+            var mapeador = FabricaGenerica.GetInstancia().CrieObjeto<IMapeadorDeMarcas>();
+
+            try
+            {
+                ServerUtils.BeginTransaction();
+                mapeador.Excluir(idMarca);
+                ServerUtils.CommitTransaction();
+            }
+            catch
+            {
+                ServerUtils.RollbackTransaction();
+                throw;
+            }
+            finally
+            {
+                ServerUtils.libereRecursos();
+            }
+        }
+    }
+}
