364241593f807a82e1a15979a9dc3ddc7833f5e8
mrmarins89 <mrmarins89@gmail.com>
2013-10-12 12:49:47 +0000

    Commit da classe faltosa

---
+++ src/MP.Negocio/PatentePrioridadeUnionista.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MP.Interfaces.Negocio;
+
+namespace MP.Negocio
+{
+    public class PatentePrioridadeUnionista : IPatentePrioridadeUnionista
+    {
+        public DateTime DataPrioridade { get; set; }
+
+        public string NumeroPrioridade { get; set; }
+
+        public int IdPais { get; set; }
+    }
+}
