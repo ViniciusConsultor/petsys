4f9328133f8ae1be1505b3c5719ffc94a50a3a34
hermessilvajunior <hermessilvajunior@gmail.com>
2013-10-14 01:29:00 +0000

    Implementação do mapeador

---
+++ src/MP.Mapeadores/MapeadorDeProcessoDeMarca.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MP.Interfaces.Mapeadores;
+using MP.Interfaces.Negocio;
+
+namespace MP.Mapeadores
+{
+    public class MapeadorDeProcessoDeMarca : IMapeadorDeProcessoDeMarca
+    {
+        public void Inserir(IProcessoDeMarca processoDeMarca)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Modificar(IProcessoDeMarca processoDeMarca)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Excluir(long ID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<IProcessoDeMarca> ObtenhaProcessosDeMarcas(int quantidadeDeRegistros, int offSet)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IProcessoDeMarca Obtenha(long ID)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- src/MP.Servicos.Local/ServicoDeProcessoDeMarcaLocal.cs
+++ src/MP.Servicos.Local/ServicoDeProcessoDeMarcaLocal.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Compartilhados;
+using Compartilhados.Fabricas;
+using MP.Interfaces.Mapeadores;
 using MP.Interfaces.Negocio;
 using MP.Interfaces.Servicos;
 
@@ -16,27 +18,106 @@ namespace MP.Servicos.Local
 
         public void Inserir(IProcessoDeMarca processoDeMarca)
         {
-            
+            ServerUtils.setCredencial(_Credencial);
+
+            var mapeador = FabricaGenerica.GetInstancia().CrieObjeto<IMapeadorDeProcessoDeMarca>();
+
+            try
+            {
+                ServerUtils.BeginTransaction();
+                mapeador.Inserir(processoDeMarca);
+                ServerUtils.CommitTransaction();
+            }
+            catch
+            {
+                ServerUtils.RollbackTransaction();
+                throw;
+            }
+            finally
+            {
+                ServerUtils.libereRecursos();
+            }
         }
 
         public void Modificar(IProcessoDeMarca processoDeMarca)
         {
-            
+            ServerUtils.setCredencial(_Credencial);
+
+            var mapeador = FabricaGenerica.GetInstancia().CrieObjeto<IMapeadorDeProcessoDeMarca>();
+
+            try
+            {
+                ServerUtils.BeginTransaction();
+                mapeador.Modificar(processoDeMarca);
+                ServerUtils.CommitTransaction();
+            }
+            catch
+            {
+                ServerUtils.RollbackTransaction();
+                throw;
+            }
+            finally
+            {
+                ServerUtils.libereRecursos();
+            }
         }
 
         public void Excluir(long ID)
         {
+            ServerUtils.setCredencial(_Credencial);
+
+            var mapeador = FabricaGenerica.GetInstancia().CrieObjeto<IMapeadorDeProcessoDeMarca>();
+
+            try
+            {
+                ServerUtils.BeginTransaction();
+                mapeador.Excluir(ID);
+                ServerUtils.CommitTransaction();
+            }
+            catch
+            {
+                ServerUtils.RollbackTransaction();
+                throw;
+            }
+            finally
+            {
+                ServerUtils.libereRecursos();
+            }
             
         }
 
         public IList<IProcessoDeMarca> ObtenhaProcessosDeMarcas(int quantidadeDeRegistros, int offSet)
         {
-            throw new NotImplementedException();
+            ServerUtils.setCredencial(_Credencial);
+
+            var mapeador = FabricaGenerica.GetInstancia().CrieObjeto<IMapeadorDeProcessoDeMarca>();
+
+            try
+            {
+                return mapeador.ObtenhaProcessosDeMarcas(quantidadeDeRegistros, offSet);
+            }
+
+            finally
+            {
+                ServerUtils.libereRecursos();
+            }
         }
 
         public IProcessoDeMarca Obtenha(long ID)
         {
-            throw new NotImplementedException();
+            ServerUtils.setCredencial(_Credencial);
+
+            var mapeador = FabricaGenerica.GetInstancia().CrieObjeto<IMapeadorDeProcessoDeMarca>();
+
+            try
+            {
+                return mapeador.Obtenha(ID);
+            }
+           
+            finally
+            {
+                ServerUtils.libereRecursos();
+            }
         }
     }
 }
