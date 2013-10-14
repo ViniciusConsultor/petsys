3acad848bcc8de9c5ea057532a92e1b91b3b05e9
hermessilvajunior@gmail.com <hermessilvajunior@gmail.com@d652af1f-c5ef-232d-d197-2330bd19a0a3>
2013-10-14 22:35:45 +0000

    Adicionando conceito de processo de patente.

---
+++ trunk/src/MP.Client/MP/cdProcessoDePatente.aspx
@@ -0,0 +1,3 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/src/MP.Client/WorkSpace.Master" AutoEventWireup="true" CodeBehind="cdProcessoDePatente.aspx.cs" Inherits="MP.Client.MP.cdProcessoDePatente" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+</asp:Content>
---
+++ trunk/src/MP.Client/MP/frmProcessosDePatentes.aspx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Telerik.Web.UI;
+
+namespace MP.Client.MP
+{
+    public partial class frmProcessosDePatentes : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void cboTipoDeFiltro_OnSelectedIndexChanged(object sender, RadComboBoxSelectedIndexChangedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected void btnPesquisarPorDataDeEntrada_OnClick(object sender, ImageClickEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected void btnPesquisarPorProcesso_OnClick(object sender, ImageClickEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected void btnPesquisarPorTituloDaPatente_OnClick(object sender, ImageClickEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected void btnPesquisarPorTipoDePatente_OnClick(object sender, ImageClickEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
