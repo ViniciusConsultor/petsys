7238265c3987e3d8f259ff8a71fbc5c19a8e78df
hermessilvajunior@gmail.com <hermessilvajunior@gmail.com@d652af1f-c5ef-232d-d197-2330bd19a0a3>
2013-10-17 23:56:56 +0000

    Adicionando mapeador de processo de patente

--- trunk/src/MP.Servicos.Local/ServicoDeProcessoDePatenteLocal.cs
+++ trunk/src/MP.Servicos.Local/ServicoDeProcessoDePatenteLocal.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Compartilhados;
+using Compartilhados.Fabricas;
 using Compartilhados.Interfaces.Core.Negocio;
+using MP.Interfaces.Mapeadores;
 using MP.Interfaces.Negocio;
 using MP.Interfaces.Servicos;
 
@@ -17,32 +19,120 @@ namespace MP.Servicos.Local
 
         public void Inserir(IProcessoDePatente processoDePatente)
         {
-            throw new NotImplementedException();
+            ServerUtils.setCredencial(_Credencial);
+
+            var mapeador = FabricaGenerica.GetInstancia().CrieObjeto<IMapeadorDeProcessoDePatente>();
+
+            try
+            {
+                ServerUtils.BeginTransaction();
+                mapeador.Inserir(processoDePatente);
+                ServerUtils.CommitTransaction();
+            }
+            catch
+            {
+                ServerUtils.RollbackTransaction();
+                throw;
+            }
+            finally
+            {
+                ServerUtils.libereRecursos();
+            }
         }
 
         public void Modificar(IProcessoDePatente processoDePatente)
         {
-            throw new NotImplementedException();
+            ServerUtils.setCredencial(_Credencial);
+
+            var mapeador = FabricaGenerica.GetInstancia().CrieObjeto<IMapeadorDeProcessoDePatente>();
+
+            try
+            {
+                ServerUtils.BeginTransaction();
+                mapeador.Modificar(processoDePatente);
+                ServerUtils.CommitTransaction();
+            }
+            catch
+            {
+                ServerUtils.RollbackTransaction();
+                throw;
+            }
+            finally
+            {
+                ServerUtils.libereRecursos();
+            }
         }
 
         public void Excluir(long ID)
         {
-            throw new NotImplementedException();
+            ServerUtils.setCredencial(_Credencial);
+
+            var mapeador = FabricaGenerica.GetInstancia().CrieObjeto<IMapeadorDeProcessoDePatente>();
+
+            try
+            {
+                ServerUtils.BeginTransaction();
+                mapeador.Excluir(ID);
+                ServerUtils.CommitTransaction();
+            }
+            catch
+            {
+                ServerUtils.RollbackTransaction();
+                throw;
+            }
+            finally
+            {
+                ServerUtils.libereRecursos();
+            }
         }
 
         public IList<IProcessoDePatente> ObtenhaProcessosDePatentes(IFiltro filtro, int quantidadeDeRegistros, int offSet)
         {
-            return new List<IProcessoDePatente>();
+            ServerUtils.setCredencial(_Credencial);
+
+            var mapeador = FabricaGenerica.GetInstancia().CrieObjeto<IMapeadorDeProcessoDePatente>();
+
+            try
+            {
+                return mapeador.ObtenhaProcessosDePatentes(filtro,quantidadeDeRegistros, offSet);
+            }
+            finally
+            {
+                ServerUtils.libereRecursos();
+            }
         }
 
         public IProcessoDePatente Obtenha(long ID)
         {
-            throw new NotImplementedException();
+            ServerUtils.setCredencial(_Credencial);
+
+            var mapeador = FabricaGenerica.GetInstancia().CrieObjeto<IMapeadorDeProcessoDePatente>();
+
+            try
+            {
+                return mapeador.Obtenha(ID);
+            }
+            finally
+            {
+                ServerUtils.libereRecursos();
+            }
+         
         }
 
         public int ObtenhaQuantidadeDeProcessosCadastrados(IFiltro filtro)
         {
-            return 0;
+            ServerUtils.setCredencial(_Credencial);
+
+            var mapeador = FabricaGenerica.GetInstancia().CrieObjeto<IMapeadorDeProcessoDePatente>();
+
+            try
+            {
+                return mapeador.ObtenhaQuantidadeDeProcessosCadastrados(filtro);
+            }
+            finally
+            {
+                ServerUtils.libereRecursos();
+            }
         }
     }
 }
