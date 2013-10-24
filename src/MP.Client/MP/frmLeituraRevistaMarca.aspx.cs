1479a0f0d22456d2ef15305112cb1673091157ec
chrisleyjms <chrisleyjms@gmail.com>
2013-10-24 23:50:35 +0000

    MP Versão: 1.0 Msg: Desenvolvimento da leitura de revistas de marca

--- src/MP.Client/MP/frmLeituraRevistaMarca.aspx.cs
+++ src/MP.Client/MP/frmLeituraRevistaMarca.aspx.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Compartilhados;
+using Compartilhados.Componentes.Web;
+using Telerik.Web.UI;
 
 namespace MP.Client.MP
 {
@@ -12,6 +16,62 @@ namespace MP.Client.MP
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void uplRevistaMarca_OnFileUploaded(object sender, FileUploadedEventArgs e)
+        {
+            try
+            {
+                if (uplRevistaMarca.UploadedFiles.Count > 0)
+                {
+                    var arquivo = uplRevistaMarca.UploadedFiles[0];
+                    var pastaDeDestino = Server.MapPath(UtilidadesWeb.URL_REVISTA_MARCA);
+                    var caminhoArquivo = Path.Combine(pastaDeDestino, arquivo.GetNameWithoutExtension() + arquivo.GetExtension());
+                    arquivo.SaveAs(caminhoArquivo);
+
+                    // pegar numero da revista e adicionar na grid de Revistas a processar
+                    //string.Concat(UtilidadesWeb.URL_REVISTA_MARCA, "/", arquivo.GetNameWithoutExtension() + arquivo.GetExtension());
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.GetInstancia().Erro("Erro ao carregar revista, exceção: ", ex);
+            }
+        }
+
+        protected void grdRevistasAProcessar_ItemCommand(object sender, GridCommandEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected void grdRevistasAProcessar_PageIndexChanged(object sender, GridPageChangedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected void grdRevistasAProcessar_ItemCreated(object sender, GridItemEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+        
+        protected void btnProcessarRevista_ButtonClick(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected void grdRevistasJaProcessadas_ItemCommand(object sender, GridCommandEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected void grdRevistasJaProcessadas_PageIndexChanged(object sender, GridPageChangedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected void grdRevistasJaProcessadas_ItemCreated(object sender, GridItemEventArgs e)
+        {
+            throw new NotImplementedException();
         }
     }
 }
