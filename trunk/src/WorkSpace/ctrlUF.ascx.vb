0c8c6498aa0495ef164c21488f1a2fb62500162e
hermessilvajunior@gmail.com <hermessilvajunior@gmail.com@d652af1f-c5ef-232d-d197-2330bd19a0a3>
2013-10-25 12:10:19 +0000

    Adicionando controle de UF.

---
+++ trunk/src/WorkSpace/ctrlUF.ascx.vb
@@ -0,0 +1,42 @@
+﻿Imports Compartilhados.Interfaces.Core.Negocio
+Imports Telerik.Web.UI
+Imports Compartilhados.Componentes.Web
+
+Public Class ctrlUF
+    Inherits UserControl
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+    End Sub
+
+    Public Sub Inicializa()
+        LimparControle()
+        CarregueCombo()
+    End Sub
+
+    Private Sub LimparControle()
+        UtilidadesWeb.LimparComponente(CType(cboUF, Control))
+        cboUF.ClearSelection()
+    End Sub
+
+    Public Property Codigo As String
+        Get
+            Return cboUF.SelectedValue
+        End Get
+        Set(ByVal value As String)
+            cboUF.SelectedValue = value
+        End Set
+    End Property
+
+    Private Sub CarregueCombo()
+        For Each estado In UF.ObtenhaTodos()
+            Dim item = New RadComboBoxItem(estado.Nome, estado.ID.ToString())
+
+            item.Attributes.Add("Sigla", estado.Sigla)
+
+            cboUF.Items.Add(item)
+            item.DataBind()
+        Next
+    End Sub
+
+End Class
