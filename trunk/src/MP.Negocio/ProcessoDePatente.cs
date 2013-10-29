151d03de0f2c5596b7166356e68887cdef7f6df4
hermessilvajunior@gmail.com <hermessilvajunior@gmail.com@d652af1f-c5ef-232d-d197-2330bd19a0a3>
2013-10-29 20:07:26 +0000

    Implementação do processo de patente.

--- trunk/src/MP.Negocio/ProcessoDePatente.cs
+++ trunk/src/MP.Negocio/ProcessoDePatente.cs
@@ -9,68 +9,20 @@ namespace MP.Negocio
     [Serializable]
     public class ProcessoDePatente : IProcessoDePatente
     {
-        public long? IdProcessoDePatente
-        {
-            get;set; 
-        }
-
-        public IPatente Patente
-        {
-            get; set; 
-        }
-
-        public string Processo
-        {
-            get; set;
-        }
-
-        public long? Protocolo
-        {
-            get; set; 
-        }
-
-        public DateTime DataDeEntrada
-        {
-            get; set;
-        }
-
-        public bool ProcessoEhDeTerceiro
-        {
-            get; set;
-        }
-
-        public DateTime? DataDaConcessao
-        {
-            get;
-            set;
-            
-        }
-
-        public IProcurador Procurador
-        {
-            get;
-            set;
-        }
-
-        public bool ProcessoEhEstrangeiro
-        {
-            get; set;
-        }
-
-        public bool Ativo
-        {
-            get; set; 
-        }
-
-        public bool EhPCT 
-        { 
-            get; set;
-        }
-
-        public string NumeroPCT
-        {
-            get; 
-            set;
-        }
+        public long? IdProcessoDePatente { get;set; }
+        public IPatente Patente {get; set;}
+        public string Processo {get; set;}
+        public DateTime DataDeEntrada {get; set;}
+        public bool ProcessoEhDeTerceiro {get; set;}
+        public DateTime? DataDaConcessao {get;set;}
+        public DateTime? DataDaPublicacao { get; set; }
+        public DateTime? DataDoDeposito { get; set; }
+        public DateTime? DataDaVigencia { get; set; }
+        public DateTime? DataDoExame { get; set; }
+        public IProcurador Procurador {get;set;}
+        public bool ProcessoEhEstrangeiro {get; set;}
+        public bool Ativo {get; set; }
+        public bool EhPCT {get; set;}
+        public string NumeroPCT {get; set;}
     }
 }
