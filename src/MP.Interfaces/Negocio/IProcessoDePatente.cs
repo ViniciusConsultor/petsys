ffeaed6ef096fb18fbcab3bf8d02f63bb137c7be
hermessilvajunior <hermessilvajunior@gmail.com>
2013-10-29 20:07:26 +0000

    Implementação do processo de patente.

--- src/MP.Interfaces/Negocio/IProcessoDePatente.cs
+++ src/MP.Interfaces/Negocio/IProcessoDePatente.cs
@@ -11,12 +11,15 @@ namespace MP.Interfaces.Negocio
         IPatente Patente { get; set; }
         string Processo { get; set; }
         DateTime DataDeEntrada { get; set; }
-        bool ProcessoEhDeTerceiro { get; set; }
         Nullable<DateTime> DataDaConcessao { get; set; }
+        Nullable<DateTime> DataDaPublicacao { get; set; }
+        Nullable<DateTime> DataDoDeposito { get; set; }
+        Nullable<DateTime> DataDaVigencia { get; set; }
+        Nullable<DateTime> DataDoExame { get; set; } 
         IProcurador Procurador { get; set; }
         bool ProcessoEhEstrangeiro { get; set; }
+        bool ProcessoEhDeTerceiro { get; set; }
         bool Ativo { get; set; }
-        bool EhPCT { get; set; }
-        string NumeroPCT { get; set; } 
+        IPCT PCT { get; set; }
     }
 }
