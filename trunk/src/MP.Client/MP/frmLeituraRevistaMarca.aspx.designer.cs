b0f9193e3d0e8c310a3853eb90a191e10480cc7b
chrisleyjms@gmail.com <chrisleyjms@gmail.com@d652af1f-c5ef-232d-d197-2330bd19a0a3>
2013-11-01 00:45:16 +0000

    Desenvolvimento da leitura de RevistaDeMarcas.

--- trunk/src/MP.Client/MP/frmLeituraRevistaMarca.aspx.designer.cs
+++ trunk/src/MP.Client/MP/frmLeituraRevistaMarca.aspx.designer.cs
@@ -103,6 +103,15 @@ namespace MP.Client.MP {
         protected global::Telerik.Web.UI.RadGrid grdRevistasJaProcessadas;
         
         /// <summary>
+        /// pnlFiltro control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel pnlFiltro;
+        
+        /// <summary>
         /// Label1 control.
         /// </summary>
         /// <remarks>
