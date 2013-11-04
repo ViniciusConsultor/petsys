720feee7aa39545c90b2fd518967c54de2dc7f93
mrmarins89@gmail.com <mrmarins89@gmail.com@d652af1f-c5ef-232d-d197-2330bd19a0a3>
2013-11-04 05:10:40 +0000

    Início da implementação da funcionalidade de leitura da revista de patente

---
+++ trunk/src/MP.Mapeadores/MapeadorDeRevistaDePatente.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MP.Interfaces.Mapeadores;
+using MP.Interfaces.Negocio;
+
+namespace MP.Mapeadores
+{
+    public class MapeadorDeRevistaDePatente : IMapeadorDeRevistaDePatente
+    {
+        public void InserirDadosRevistaXml(IList<IRevistaDePatente> listaDeProcessosExistentesNaRevista)
+        {
+        }
+
+        public void Modificar(IRevistaDePatente revistaDePatentes)
+        {
+        }
+
+        public IList<IRevistaDePatente> ObtenhaRevistasAProcessar(int quantidadeDeRegistros)
+        {
+            return new List<IRevistaDePatente>();
+        }
+
+        public IList<IRevistaDePatente> ObtenhaRevistasJaProcessadas(int quantidadeDeRegistros)
+        {
+            return new List<IRevistaDePatente>();
+        }
+    }
+}
---
+++ trunk/src/MP.Servicos.Local/ServicoDeRevistaDePatenteLocal.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Compartilhados;
+using MP.Interfaces.Negocio;
+using MP.Interfaces.Servicos;
+
+namespace MP.Servicos.Local
+{
+    public class ServicoDeRevistaDePatenteLocal : Servico, IServicoDeRevistaDePatente
+    {
+        public ServicoDeRevistaDePatenteLocal(ICredencial Credencial) : base(Credencial)
+        {
+        }
+
+        public void Inserir(IList<IRevistaDePatente> listaDeObjetoRevistaDeMarcas)
+        {
+        }
+
+        public void Modificar(IRevistaDePatente revistaDeMarcas)
+        {
+        }
+
+        public IList<IRevistaDePatente> ObtenhaRevistasAProcessar(int quantidadeDeRegistros)
+        {
+            return new List<IRevistaDePatente>();
+        }
+
+        public IList<IRevistaDePatente> ObtenhaRevistasJaProcessadas(int quantidadeDeRegistros)
+        {
+            return new List<IRevistaDePatente>();
+        }
+    }
+}
