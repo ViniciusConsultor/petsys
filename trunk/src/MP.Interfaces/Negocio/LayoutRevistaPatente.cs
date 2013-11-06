1cb905f1fe102ad6105c934ef5358707993ef185
mrmarins89@gmail.com <mrmarins89@gmail.com@d652af1f-c5ef-232d-d197-2330bd19a0a3>
2013-11-06 14:43:27 +0000

    Implementação parcial da funcionalidade de leitura da revista de patente

--- trunk/src/MP.Interfaces/Negocio/LayoutRevistaPatente.cs
+++ trunk/src/MP.Interfaces/Negocio/LayoutRevistaPatente.cs
@@ -12,12 +12,14 @@ namespace MP.Interfaces.Negocio
         private string descricaoCampo;
 
         public static LayoutRevistaPatente Layout11 = new LayoutRevistaPatente("(11)", "Número da Patente");
+        public static LayoutRevistaPatente Layout15 = new LayoutRevistaPatente("(15)", "Data do Registro/Data da Prorrogação");
         public static LayoutRevistaPatente Layout21 = new LayoutRevistaPatente("(21)", "Número do Pedido");
         public static LayoutRevistaPatente Layout22 = new LayoutRevistaPatente("(22)", "Data do Depósito");
         public static LayoutRevistaPatente Layout30 = new LayoutRevistaPatente("(30)", "Dados da Prioridade Unionista (data de depósito, país, número)");
         public static LayoutRevistaPatente Layout43 = new LayoutRevistaPatente("(43)", "Data da Publicação do Pedido");
         public static LayoutRevistaPatente Layout45 = new LayoutRevistaPatente("(45)", "Data da Concessão da Patente/Certificado de Adição de Invenção");
         public static LayoutRevistaPatente Layout51 = new LayoutRevistaPatente("(51)", "Classificação Internacional");
+        public static LayoutRevistaPatente Layout52 = new LayoutRevistaPatente("(52)", "Classificação Nacional");
         public static LayoutRevistaPatente Layout54 = new LayoutRevistaPatente("(54)", "Título");
         public static LayoutRevistaPatente Layout57 = new LayoutRevistaPatente("(57)", "Resumo");
         public static LayoutRevistaPatente Layout61 = new LayoutRevistaPatente("(61)", "Dados do Pedido ou patente principal do qual o presente é uma adição (número e data de depósito)");
@@ -30,7 +32,36 @@ namespace MP.Interfaces.Negocio
         public static LayoutRevistaPatente Layout81 = new LayoutRevistaPatente("(81)", "Países Designados");
         public static LayoutRevistaPatente Layout85 = new LayoutRevistaPatente("(85)", "Data do Início da Fase Nacional");
         public static LayoutRevistaPatente Layout86 = new LayoutRevistaPatente("(86)", "Número, Idioma e Data do Depósito Internacional");
-        public static LayoutRevistaPatente Layout87 = new LayoutRevistaPatente("(87)", "Número, Idioma e Data da Publicação Internacional"); 
+        public static LayoutRevistaPatente Layout87 = new LayoutRevistaPatente("(87)", "Número, Idioma e Data da Publicação Internacional");
+        public static LayoutRevistaPatente LayoutCD = new LayoutRevistaPatente("(CD)", "Código Despacho");
+        public static LayoutRevistaPatente LayoutRP = new LayoutRevistaPatente("(RP)", "Responsável pelo pagamento do Imposto de Renda");
+        public static LayoutRevistaPatente LayoutCO = new LayoutRevistaPatente("(CO)", "Complemento");
+        public static LayoutRevistaPatente LayoutDE = new LayoutRevistaPatente("(DE)", "Decisão");
+        public static LayoutRevistaPatente LayoutRE = new LayoutRevistaPatente("(RE)", "Recorrente");
+        public static LayoutRevistaPatente LayoutNP = new LayoutRevistaPatente("(NP)", "Número do Processo");
+        public static LayoutRevistaPatente LayoutCE = new LayoutRevistaPatente("(CE)", "Cedente");
+        public static LayoutRevistaPatente LayoutCS = new LayoutRevistaPatente("(CS)", "Cessionária");
+        public static LayoutRevistaPatente LayoutOB = new LayoutRevistaPatente("(OB)", "Objeto");
+        public static LayoutRevistaPatente LayoutUI = new LayoutRevistaPatente("(UI)", "Com última informação");
+        public static LayoutRevistaPatente LayoutCA = new LayoutRevistaPatente("(CA)", "Certificado de averbação");
+        public static LayoutRevistaPatente LayoutPE = new LayoutRevistaPatente("(PE)", "País Cedente");
+        public static LayoutRevistaPatente LayoutPS = new LayoutRevistaPatente("(PS)", "País da Concessionária");
+        public static LayoutRevistaPatente LayoutSE = new LayoutRevistaPatente("(SE)", "Setor");
+        public static LayoutRevistaPatente LayoutES = new LayoutRevistaPatente("(ES)", "Endereço da Concessionária");
+        public static LayoutRevistaPatente LayoutND = new LayoutRevistaPatente("(ND)", "Natureza do documento");
+        public static LayoutRevistaPatente LayoutMO = new LayoutRevistaPatente("(MO)", "Moeda de pagamento");
+        public static LayoutRevistaPatente LayoutVA = new LayoutRevistaPatente("(VA)", "Valor");
+        public static LayoutRevistaPatente LayoutPG = new LayoutRevistaPatente("(PG)", "Pagamento");
+        public static LayoutRevistaPatente LayoutPZ = new LayoutRevistaPatente("(PZ)", "Prazo");
+        public static LayoutRevistaPatente LayoutIA = new LayoutRevistaPatente("(IA)", "Serviços/Despesas Isentas de Averbação");
+        public static LayoutRevistaPatente LayoutCR = new LayoutRevistaPatente("(CR)", "Criador");
+        public static LayoutRevistaPatente LayoutLG = new LayoutRevistaPatente("(LG)", "Linguagem");
+        public static LayoutRevistaPatente LayoutCP = new LayoutRevistaPatente("(CP)", "Campo de aplicação");
+        public static LayoutRevistaPatente LayoutTP = new LayoutRevistaPatente("(TP)", "Tipo de programa");
+        public static LayoutRevistaPatente LayoutDL = new LayoutRevistaPatente("(DL)", "Data da criação");
+        public static LayoutRevistaPatente LayoutRG = new LayoutRevistaPatente("(RG)", "Regime Guarda");
+        public static LayoutRevistaPatente LayoutRQ = new LayoutRevistaPatente("(RQ)", "Requerente");
+        public static LayoutRevistaPatente LayoutRD = new LayoutRevistaPatente("(RD)", "Redaçao");
 
         private static IList<LayoutRevistaPatente> layoutsRevistaPatente = new List<LayoutRevistaPatente>();
 
---
+++ trunk/src/MP.Interfaces/Utilidades/TradutorDeRevistaPatenteTXTParaRevistaPatenteXML.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MP.Interfaces.Negocio;
+
+namespace MP.Interfaces.Utilidades
+{
+    public class TradutorDeRevistaPatenteTXTParaRevistaPatenteXML
+    {
+        private void PreenchaObjetoDTOLayoutLeituraRevistaPatente(string linha, ref DTOLayoutLeituraRevistaPatente dtoLayoutLeituraRevistaPatente,
+            ref string ultimaTagPreenchida)
+        {
+            if (linha.Length < 4)
+                return;
+
+            if (dtoLayoutLeituraRevistaPatente == null)
+                dtoLayoutLeituraRevistaPatente = new DTOLayoutLeituraRevistaPatente();
+
+            string valorDaLinha = linha.Remove(0, 4);
+
+            dtoLayoutLeituraRevistaPatente.NumeroDaPatente = VerifiqueTagAtualEPreenchaObjeto(linha.Substring(0, 4), LayoutRevistaPatente.Layout11.IdentificadorCampo, ultimaTagPreenchida, valorDaLinha);
+            dtoLayoutLeituraRevistaPatente.DataDaProrrogacao = VerifiqueTagAtualEPreenchaObjeto(linha.Substring(0, 4), LayoutRevistaPatente.Layout15.IdentificadorCampo, ultimaTagPreenchida, DateTime.Parse(valorDaLinha.Trim()));
+            dtoLayoutLeituraRevistaPatente.NumeroDoPedido = VerifiqueTagAtualEPreenchaObjeto(linha.Substring(0, 4), LayoutRevistaPatente.Layout21.IdentificadorCampo, ultimaTagPreenchida, valorDaLinha);
+            dtoLayoutLeituraRevistaPatente.DataDoDeposito = VerifiqueTagAtualEPreenchaObjeto(linha.Substring(0, 4), LayoutRevistaPatente.Layout22.IdentificadorCampo, ultimaTagPreenchida, DateTime.Parse(valorDaLinha.Trim()));
+            dtoLayoutLeituraRevistaPatente.PrioridadeUnionista = VerifiqueTagAtualEPreenchaObjeto(linha.Substring(0, 4), LayoutRevistaPatente.Layout30.IdentificadorCampo, ultimaTagPreenchida, valorDaLinha.Trim());
+            dtoLayoutLeituraRevistaPatente.DataDaPublicacaoDoPedido = VerifiqueTagAtualEPreenchaObjeto(linha.Substring(0, 4), LayoutRevistaPatente.Layout43.IdentificadorCampo, ultimaTagPreenchida, DateTime.Parse(valorDaLinha.Trim()));
+            dtoLayoutLeituraRevistaPatente.DataDeConcessao = VerifiqueTagAtualEPreenchaObjeto(linha.Substring(0, 4), LayoutRevistaPatente.Layout45, ultimaTagPreenchida.IdentificadorCampo, DateTime.Parse(valorDaLinha.Trim()));
+            dtoLayoutLeituraRevistaPatente.ClassificacaoInternacional = VerifiqueTagAtualEPreenchaObjeto(linha.Substring(0, 4), LayoutRevistaPatente.Layout51.IdentificadorCampo, ultimaTagPreenchida, valorDaLinha);
+            dtoLayoutLeituraRevistaPatente.ClassificacaoNacional = VerifiqueTagAtualEPreenchaObjeto(linha.Substring(0, 4), LayoutRevistaPatente.Layout52.IdentificadorCampo, ultimaTagPreenchida, valorDaLinha);
+            dtoLayoutLeituraRevistaPatente.Titulo = VerifiqueTagAtualEPreenchaObjeto(linha.Substring(0, 4), LayoutRevistaPatente.Layout54.IdentificadorCampo, ultimaTagPreenchida, valorDaLinha);
+            dtoLayoutLeituraRevistaPatente.Resumo = VerifiqueTagAtualEPreenchaObjeto(linha.Substring(0, 4), LayoutRevistaPatente.Layout57.IdentificadorCampo, ultimaTagPreenchida, valorDaLinha);
+            dtoLayoutLeituraRevistaPatente.DadosDoPedidoDaPatente = VerifiqueTagAtualEPreenchaObjeto(linha.Substring(0, 4), LayoutRevistaPatente.Layout61.IdentificadorCampo, ultimaTagPreenchida, valorDaLinha);
+            dtoLayoutLeituraRevistaPatente.DadosDoPedidoOriginal = VerifiqueTagAtualEPreenchaObjeto(linha.Substring(0, 4), LayoutRevistaPatente.Layout62.IdentificadorCampo, ultimaTagPreenchida, valorDaLinha);
+            dtoLayoutLeituraRevistaPatente.PrioridadeInterna = VerifiqueTagAtualEPreenchaObjeto(linha.Substring(0, 4), LayoutRevistaPatente.Layout66.IdentificadorCampo, ultimaTagPreenchida, valorDaLinha);
+            dtoLayoutLeituraRevistaPatente.Depositante = VerifiqueTagAtualEPreenchaObjeto(linha.Substring(0, 4), LayoutRevistaPatente.Layout71.IdentificadorCampo, ultimaTagPreenchida, valorDaLinha);
+            dtoLayoutLeituraRevistaPatente.Inventor = VerifiqueTagAtualEPreenchaObjeto(linha.Substring(0, 4), LayoutRevistaPatente.Layout72.IdentificadorCampo, ultimaTagPreenchida, valorDaLinha);
+            dtoLayoutLeituraRevistaPatente.Titular = VerifiqueTagAtualEPreenchaObjeto(linha.Substring(0, 4), LayoutRevistaPatente.Layout73.IdentificadorCampo, ultimaTagPreenchida, valorDaLinha);
+            dtoLayoutLeituraRevistaPatente.Procurador = VerifiqueTagAtualEPreenchaObjeto(linha.Substring(0, 4), LayoutRevistaPatente.Layout74.IdentificadorCampo, ultimaTagPreenchida, valorDaLinha);
+            dtoLayoutLeituraRevistaPatente.PaisesDesignados = VerifiqueTagAtualEPreenchaObjeto(linha.Substring(0, 4), LayoutRevistaPatente.Layout81.IdentificadorCampo, ultimaTagPreenchida, valorDaLinha);
+            dtoLayoutLeituraRevistaPatente.DataInicioFaseNacional = VerifiqueTagAtualEPreenchaObjeto(linha.Substring(0, 4), LayoutRevistaPatente.Layout85.IdentificadorCampo, ultimaTagPreenchida, DateTime.Parse(valorDaLinha.Trim()));
+            dtoLayoutLeituraRevistaPatente.DadosDepositoInternacional = VerifiqueTagAtualEPreenchaObjeto(linha.Substring(0, 4), LayoutRevistaPatente.Layout86.IdentificadorCampo, ultimaTagPreenchida, valorDaLinha);
+            dtoLayoutLeituraRevistaPatente.DadosPublicacaoInternacional = VerifiqueTagAtualEPreenchaObjeto(linha.Substring(0, 4), LayoutRevistaPatente.Layout87.IdentificadorCampo, ultimaTagPreenchida, valorDaLinha);
+            dtoLayoutLeituraRevistaPatente.CodigoDoDespacho = VerifiqueTagAtualEPreenchaObjeto(linha.Substring(0, 4), LayoutRevistaPatente.LayoutCD.IdentificadorCampo, ultimaTagPreenchida, valorDaLinha);
+            dtoLayoutLeituraRevistaPatente.ResponsavelPagamentoImpostoDeRenda = VerifiqueTagAtualEPreenchaObjeto(linha.Substring(0, 4), LayoutRevistaPatente.LayoutRP.IdentificadorCampo, ultimaTagPreenchida, valorDaLinha);
+            dtoLayoutLeituraRevistaPatente.Complemento = VerifiqueTagAtualEPreenchaObjeto(linha.Substring(0, 4), LayoutRevistaPatente.LayoutCO.IdentificadorCampo, ultimaTagPreenchida, valorDaLinha);
+            dtoLayoutLeituraRevistaPatente.Decisao = VerifiqueTagAtualEPreenchaObjeto(linha.Substring(0, 4), LayoutRevistaPatente.LayoutDE.IdentificadorCampo, ultimaTagPreenchida, valorDaLinha);
+            dtoLayoutLeituraRevistaPatente.Recorrente = VerifiqueTagAtualEPreenchaObjeto(linha.Substring(0, 4), LayoutRevistaPatente.LayoutRE.IdentificadorCampo, ultimaTagPreenchida, valorDaLinha);
+            dtoLayoutLeituraRevistaPatente.NumeroDoProcesso = VerifiqueTagAtualEPreenchaObjeto(linha.Substring(0, 4), LayoutRevistaPatente.LayoutNP.IdentificadorCampo, ultimaTagPreenchida, valorDaLinha);
+            dtoLayoutLeituraRevistaPatente.Cedente = VerifiqueTagAtualEPreenchaObjeto(linha.Substring(0, 4), LayoutRevistaPatente.LayoutCE.IdentificadorCampo, ultimaTagPreenchida, valorDaLinha);
+            dtoLayoutLeituraRevistaPatente.Cessionaria = VerifiqueTagAtualEPreenchaObjeto(linha.Substring(0, 4), LayoutRevistaPatente.LayoutCS.IdentificadorCampo, ultimaTagPreenchida, valorDaLinha);
+            dtoLayoutLeituraRevistaPatente.Objeto = VerifiqueTagAtualEPreenchaObjeto(linha.Substring(0, 4), LayoutRevistaPatente.LayoutOB.IdentificadorCampo, ultimaTagPreenchida, valorDaLinha);
+            dtoLayoutLeituraRevistaPatente.UltimaInformacao = VerifiqueTagAtualEPreenchaObjeto(linha.Substring(0, 4), LayoutRevistaPatente.LayoutUI.IdentificadorCampo, ultimaTagPreenchida, valorDaLinha);
+            dtoLayoutLeituraRevistaPatente.CertificadoDeAverbacao = VerifiqueTagAtualEPreenchaObjeto(linha.Substring(0, 4), LayoutRevistaPatente.LayoutCA.IdentificadorCampo, ultimaTagPreenchida, valorDaLinha);
+            dtoLayoutLeituraRevistaPatente.PaisCedente = VerifiqueTagAtualEPreenchaObjeto(linha.Substring(0, 4), LayoutRevistaPatente.LayoutPE.IdentificadorCampo, ultimaTagPreenchida, valorDaLinha);
+            dtoLayoutLeituraRevistaPatente.PaisDaCessionaria = VerifiqueTagAtualEPreenchaObjeto(linha.Substring(0, 4), LayoutRevistaPatente.LayoutPS.IdentificadorCampo, ultimaTagPreenchida, valorDaLinha);
+            dtoLayoutLeituraRevistaPatente.Setor = VerifiqueTagAtualEPreenchaObjeto(linha.Substring(0, 4), LayoutRevistaPatente.LayoutSE.IdentificadorCampo, ultimaTagPreenchida, valorDaLinha);
+            dtoLayoutLeituraRevistaPatente.EnderecoDaCessionaria = VerifiqueTagAtualEPreenchaObjeto(linha.Substring(0, 4), LayoutRevistaPatente.LayoutES.IdentificadorCampo, ultimaTagPreenchida, valorDaLinha);
+            dtoLayoutLeituraRevistaPatente.NaturezaDoDocumento = VerifiqueTagAtualEPreenchaObjeto(linha.Substring(0, 4), LayoutRevistaPatente.LayoutND.IdentificadorCampo, ultimaTagPreenchida, valorDaLinha);
+            dtoLayoutLeituraRevistaPatente.MoedaDePagamento = VerifiqueTagAtualEPreenchaObjeto(linha.Substring(0, 4), LayoutRevistaPatente.LayoutMO.IdentificadorCampo, ultimaTagPreenchida, valorDaLinha);
+            dtoLayoutLeituraRevistaPatente.Valor = VerifiqueTagAtualEPreenchaObjeto(linha.Substring(0, 4), LayoutRevistaPatente.LayoutVA.IdentificadorCampo, ultimaTagPreenchida, valorDaLinha);
+            dtoLayoutLeituraRevistaPatente.Pagamento = VerifiqueTagAtualEPreenchaObjeto(linha.Substring(0, 4), LayoutRevistaPatente.LayoutPG.IdentificadorCampo, ultimaTagPreenchida, valorDaLinha);
+            dtoLayoutLeituraRevistaPatente.Prazo = VerifiqueTagAtualEPreenchaObjeto(linha.Substring(0, 4), LayoutRevistaPatente.LayoutPZ.IdentificadorCampo, ultimaTagPreenchida, valorDaLinha);
+            dtoLayoutLeituraRevistaPatente.ServicosIsentosDeAverbacao = VerifiqueTagAtualEPreenchaObjeto(linha.Substring(0, 4), LayoutRevistaPatente.LayoutIA.IdentificadorCampo, ultimaTagPreenchida, valorDaLinha);
+            dtoLayoutLeituraRevistaPatente.Criador = VerifiqueTagAtualEPreenchaObjeto(linha.Substring(0, 4), LayoutRevistaPatente.LayoutCR.IdentificadorCampo, ultimaTagPreenchida, valorDaLinha);
+            dtoLayoutLeituraRevistaPatente.Linguagem = VerifiqueTagAtualEPreenchaObjeto(linha.Substring(0, 4), LayoutRevistaPatente.LayoutLG.IdentificadorCampo, ultimaTagPreenchida, valorDaLinha);
+            dtoLayoutLeituraRevistaPatente.CampoDeAplicacao = VerifiqueTagAtualEPreenchaObjeto(linha.Substring(0, 4), LayoutRevistaPatente.LayoutCP.IdentificadorCampo, ultimaTagPreenchida, valorDaLinha);
+            dtoLayoutLeituraRevistaPatente.TipoDePrograma = VerifiqueTagAtualEPreenchaObjeto(linha.Substring(0, 4), LayoutRevistaPatente.LayoutTP.IdentificadorCampo, ultimaTagPreenchida, valorDaLinha);
+            dtoLayoutLeituraRevistaPatente.DataDaCriacao = VerifiqueTagAtualEPreenchaObjeto(linha.Substring(0, 4), LayoutRevistaPatente.LayoutCR.IdentificadorCampo, ultimaTagPreenchida, DateTime.Parse(valorDaLinha.Trim()));
+            dtoLayoutLeituraRevistaPatente.RegimeDeGuarda = VerifiqueTagAtualEPreenchaObjeto(linha.Substring(0, 4), LayoutRevistaPatente.LayoutRG.IdentificadorCampo, ultimaTagPreenchida, valorDaLinha);
+            dtoLayoutLeituraRevistaPatente.Requerente = VerifiqueTagAtualEPreenchaObjeto(linha.Substring(0, 4), LayoutRevistaPatente.LayoutRQ.IdentificadorCampo, ultimaTagPreenchida, valorDaLinha);
+            dtoLayoutLeituraRevistaPatente.Redacao = VerifiqueTagAtualEPreenchaObjeto(linha.Substring(0, 4), LayoutRevistaPatente.LayoutRD.IdentificadorCampo, ultimaTagPreenchida, valorDaLinha);
+        }
+
+        private string VerifiqueTagAtualEPreenchaObjeto(string tagAtual, string identificadorLayout, string ultimaTagAvaliada, string valorLinha)
+        {
+            if (tagAtual.Equals(identificadorLayout) || identificadorLayout.Equals(ultimaTagAvaliada))
+                return valorLinha;
+
+            return string.Empty;
+        }
+
+        private DateTime? VerifiqueTagAtualEPreenchaObjeto(string tagAtual, string identificadorLayout, string ultimaTagAvaliada, DateTime valorLinha)
+        {
+            if (tagAtual.Equals(identificadorLayout) || identificadorLayout.Equals(ultimaTagAvaliada))
+                return valorLinha;
+
+            return null;
+        }
+    }
+}
