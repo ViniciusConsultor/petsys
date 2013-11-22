432bc61d1177586b453f851ff3b700e3bbbabe08
hermessilvajunior <hermessilvajunior@gmail.com>
2013-11-22 13:39:16 +0000

    Modificações para permitir agrupar funcionalidades no menu

--- src/Core.Interfaces/Negocio/IMenuComposto.vb
+++ src/Core.Interfaces/Negocio/IMenuComposto.vb
@@ -2,10 +2,11 @@
 
     Public Interface IMenuComposto
         Inherits IMenuAbstrato
-
+        
+        Property Agrupador As IDictionary(Of String, IMenuComposto)
         Function ObtenhaItens() As IList(Of IMenuAbstrato)
         Sub AdicioneItem(ByVal Item As IMenuAbstrato)
-
+        
     End Interface
 
 End Namespace
--- src/Core.Mapeadores/MapeadorDeMenu.vb
+++ src/Core.Mapeadores/MapeadorDeMenu.vb
@@ -14,12 +14,12 @@ Public Class MapeadorDeMenu
         Dim DBHelper As IDBHelper
 
         SQL.AppendLine("SELECT NCL_MODULO.NOME AS NOME_MODULO, NCL_MENUMODULO.IDMODULO AS ID_MODULO, NCL_MENUMODULO.IMAGEM AS IMAGEM_MODULO, ")
-        SQL.AppendLine("NCL_MENUFUNCAO.IDFUNCAO AS ID_FUNCAO, NCL_FUNCAO.NOME AS NOME_FUNCAO, NCL_MENUFUNCAO.IMAGEM AS IMAGEM_FUNCAO, NCL_MENUFUNCAO.URL ")
+        SQL.AppendLine("NCL_MENUFUNCAO.IDFUNCAO AS ID_FUNCAO, NCL_FUNCAO.NOME AS NOME_FUNCAO, NCL_MENUFUNCAO.IMAGEM AS IMAGEM_FUNCAO, NCL_MENUFUNCAO.URL, NCL_MENUFUNCAO.AGRUPADOR")
         SQL.AppendLine(" FROM NCL_MODULO, NCL_FUNCAO, NCL_MENUMODULO, NCL_MENUFUNCAO")
         SQL.AppendLine(" WHERE NCL_MENUMODULO.IDMODULO = NCL_MODULO.IDMODULO")
         SQL.AppendLine(" AND NCL_MENUFUNCAO.IDFUNCAO = NCL_FUNCAO.IDFUNCAO")
         SQL.AppendLine(" AND NCL_MENUFUNCAO.IDMODULO = NCL_MENUMODULO.IDMODULO")
-        SQL.AppendLine(" ORDER BY NCL_MENUMODULO.IDMODULO, NCL_MENUFUNCAO.IDFUNCAO")
+        SQL.AppendLine(" ORDER BY NCL_MENUMODULO.IDMODULO, NCL_MENUFUNCAO.IDFUNCAO, NCL_MENUFUNCAO.AGRUPADOR")
 
         DBHelper = ServerUtils.criarNovoDbHelper
         Menu = FabricaGenerica.GetInstancia.CrieObjeto(Of IMenuComposto)()
@@ -31,7 +31,7 @@ Public Class MapeadorDeMenu
 
                 Dim IdModuloCorrente As String = ""
                 Dim IdFuncaoCorrente As String = ""
-
+                
                 While Leitor.Read
 
                     If IdModuloCorrente <> UtilidadesDePersistencia.GetValorString(Leitor, "ID_MODULO") Then
@@ -44,12 +44,23 @@ Public Class MapeadorDeMenu
                     End If
 
                     If IdFuncaoCorrente <> UtilidadesDePersistencia.GetValorString(Leitor, "ID_FUNCAO") Then
+                        
                         MenuFolha = FabricaGenerica.GetInstancia.CrieObjeto(Of IMenuFolha)()
                         MenuFolha.ID = UtilidadesDePersistencia.GetValorString(Leitor, "ID_FUNCAO")
                         MenuFolha.Nome = UtilidadesDePersistencia.GetValorString(Leitor, "NOME_FUNCAO")
                         MenuFolha.URL = UtilidadesDePersistencia.GetValorString(Leitor, "URL")
                         MenuFolha.Imagem = UtilidadesDePersistencia.GetValorString(Leitor, "IMAGEM_FUNCAO")
-                        MenuComposto.AdicioneItem(MenuFolha)
+
+                        If UtilidadesDePersistencia.EhNulo(Leitor, "AGRUPADOR") Then
+                            MenuComposto.AdicioneItem(MenuFolha)
+                        Else
+                            If Not MenuComposto.Agrupador.ContainsKey(UtilidadesDePersistencia.GetValorString(Leitor, "AGRUPADOR")) Then
+                                MenuComposto.Agrupador.Add(UtilidadesDePersistencia.GetValorString(Leitor, "AGRUPADOR"), FabricaGenerica.GetInstancia.CrieObjeto(Of IMenuComposto)())
+                            End If
+
+                            MenuComposto.Agrupador(UtilidadesDePersistencia.GetValorString(Leitor, "AGRUPADOR")).AdicioneItem(MenuFolha)
+                        End If
+
                         IdFuncaoCorrente = UtilidadesDePersistencia.GetValorString(Leitor, "ID_FUNCAO")
                     End If
 
