7897016246d6c1ab0130b4494c7f129ababb817a
mrmarins89@gmail.com <mrmarins89@gmail.com@d652af1f-c5ef-232d-d197-2330bd19a0a3>
2014-01-13 19:30:22 +0000

    Implementação parcial do cadastro do titular

---
+++ trunk/src/MP.Mapeadores/MapeadorDeTitular.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Compartilhados.Interfaces.Core.Negocio;
+using MP.Interfaces.Mapeadores;
+using MP.Interfaces.Negocio;
+
+namespace MP.Mapeadores
+{
+    public class MapeadorDeTitular : IMapeadorDeTitular
+    {
+        public void Inserir(ITitular inventor)
+        {
+        }
+
+        public void Remover(long ID)
+        {
+        }
+
+        public void Atualizar(ITitular inventor)
+        {
+        }
+
+        public IList<ITitular> ObtenhaPorNomeComoFiltro(string nome, int quantidadeMaxima)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ITitular Obtenha(long ID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ITitular Obtenha(IPessoa pessoa)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
