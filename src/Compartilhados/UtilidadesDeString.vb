f104e4725d4d3a86bd9d8125f8e493377c12c0bd
hermessilvajunior <hermessilvajunior@gmail.com>
2014-01-24 16:47:49 +0000

    Alteração do método para retirar acentos.

--- src/Compartilhados/UtilidadesDeString.vb
+++ src/Compartilhados/UtilidadesDeString.vb
@@ -1,12 +1,25 @@
 ﻿Imports System.Text
 Imports System.Web
+Imports System.Globalization
 
 Public Class UtilidadesDeString
     Public Shared Function RemoveAcentos(Valor As String) As String
-        If Not String.IsNullOrEmpty(Valor) Then
-            Return HttpUtility.UrlEncode(Valor, Encoding.GetEncoding(28597)).Replace("+", " ")
-        End If
+        'If Not String.IsNullOrEmpty(Valor) Then
+        '    Return HttpUtility.UrlEncode(Valor, Encoding.GetEncoding(28597)).Replace("+", " ")
+        'End If
 
-        Return Valor
+        Dim s = Valor.Normalize(NormalizationForm.FormD)
+
+        Dim sb = New StringBuilder()
+
+        For Each k As Char In s
+            Dim uc = CharUnicodeInfo.GetUnicodeCategory(k)
+            If Not uc = UnicodeCategory.NonSpacingMark Then
+                sb.Append(k)
+            End If
+        Next
+
+        Return sb.ToString()
+
     End Function
 End Class
