c4dbc35526e61b59adda51233c4a48545bd920f5
hermessilvajunior@gmail.com <hermessilvajunior@gmail.com@d652af1f-c5ef-232d-d197-2330bd19a0a3>
2014-02-04 10:33:24 +0000

    Comitando classe faltosa da refatoração

---
+++ trunk/src/MP.Client/Util.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MP.Interfaces.Negocio;
+
+namespace MP.Client
+{
+    public class Util
+    {
+
+        public static bool PeriodoEhTrimestreSemestreOuAnual(Periodo periodo)
+        {
+           if (periodo == null) return false;
+
+            return (periodo.Equals(Periodo.Trimestral) || periodo.Equals(Periodo.Semestral) ||
+                    periodo.Equals(Periodo.Anual));
+
+        }
+    }
+}
