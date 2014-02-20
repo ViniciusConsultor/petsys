3023c7bee2c4765ec9b7da5ef77ef11fb7d8c9e5
hermessilvajunior <hermessilvajunior@gmail.com>
2014-02-20 19:13:33 +0000

    Adicionando conceito de configuração de indice financeiro no modulo financeiro;.

---
+++ src/FN.Interfaces/Mapeadores/IMapeadorConfiguracaoDeIndicesFinanceiros.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Compartilhados.Interfaces.FN.Negocio;
+
+namespace FN.Interfaces.Mapeadores
+{
+    public interface IMapeadorConfiguracaoDeIndicesFinanceiros
+    {
+        IConfiguracaoDeIndicesFinanceiros ObtenhaConfiguracao();
+        void Salve(IConfiguracaoDeIndicesFinanceiros configuracao);
+    }
+}
