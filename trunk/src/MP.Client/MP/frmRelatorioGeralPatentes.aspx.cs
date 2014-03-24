90103eb7d98ea7a75ec241545bcd02d15ce90548
mrmarins89@gmail.com <mrmarins89@gmail.com@d652af1f-c5ef-232d-d197-2330bd19a0a3>
2014-03-24 01:51:37 +0000

    Implementação paricla do relatório geral de patentes

--- trunk/src/MP.Client/MP/frmRelatorioGeralPatentes.aspx.cs
+++ trunk/src/MP.Client/MP/frmRelatorioGeralPatentes.aspx.cs
@@ -4,14 +4,19 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Telerik.Web.UI;
 
 namespace MP.Client.MP
 {
-    public partial class frmRelatorioGeralPatentes : System.Web.UI.Page
+    public partial class frmRelatorioGeralPatentes : Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void rtbToolBar_ButtonClick(object sender, RadToolBarEventArgs e)
+        {
         }
     }
 }
