7e29c62339d5a6bc2698ee5326647a7417849d94
mrmarins89@gmail.com <mrmarins89@gmail.com@d652af1f-c5ef-232d-d197-2330bd19a0a3>
2014-04-03 03:09:52 +0000

    Implementação parcial do relatório geral de patentes

--- trunk/src/MP.Client/Relatorios/Patentes/GeradorDeRelatorioGeralDePatentes.cs
+++ trunk/src/MP.Client/Relatorios/Patentes/GeradorDeRelatorioGeralDePatentes.cs
@@ -1,11 +1,198 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Web;
+using System.Text;
+using System.Web;z
+using Compartilhados;
+using Compartilhados.Componentes.Web;
+using Compartilhados.Fabricas;
+using Compartilhados.Interfaces.Core.Negocio;
+using Compartilhados.Interfaces.Core.Servicos;
+using MP.Interfaces.Negocio;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
 
 namespace MP.Client.Relatorios.Patentes
 {
+    public enum OrdenacaoRelatorioGeralPatente
+    {
+        Cliente,
+        Patente
+    }
+
     public class GeradorDeRelatorioGeralDePatentes
     {
+        private Document _documento;
+        private Font _Fonte1;
+        private Font _Fonte2;
+        private Font _Fonte3;
+        private Font _Fonte4;
+        private IEmpresa empresa;
+        private IList<IProcessoDePatente> _processosDePatentes;
+
+        public GeradorDeRelatorioGeralDePatentes(IList<IProcessoDePatente> processosDePatentes)
+        {
+            _processosDePatentes = processosDePatentes;
+            _Fonte1 = new Font(Font.TIMES_ROMAN, 10);
+            _Fonte2 = new Font(Font.TIMES_ROMAN, 10, Font.BOLD);
+            _Fonte3 = new Font(Font.TIMES_ROMAN, 14, Font.BOLDITALIC);
+            _Fonte4 = new Font(Font.TIMES_ROMAN, 10, Font.BOLDITALIC);
+        }
+
+        public string GereRelatorio(string numeroDaRevistaSelecionada, string dataPublicacao)
+        {
+            var nomeDoArquivoDeSaida = GereInformacoesUteisParaOArquivoDeSaidaDoRelatorio();
+
+            _documento.Open();
+            MonteDocumentoDoRelatorio();
+            _documento.Close();
+            return nomeDoArquivoDeSaida;
+        }
+
+        private string GereInformacoesUteisParaOArquivoDeSaidaDoRelatorio()
+        {
+            using (var servico = FabricaGenerica.GetInstancia().CrieObjeto<IServicoDeEmpresa>())
+                empresa = servico.Obtenha(FabricaDeContexto.GetInstancia().GetContextoAtual().EmpresaLogada.ID);
+
+            var nomeDoArquivoDeSaida = String.Concat(DateTime.Now.ToString("yyyyMMddhhmmss"), ".pdf");
+            var caminho = String.Concat(HttpContext.Current.Request.PhysicalApplicationPath, UtilidadesWeb.PASTA_LOADS);
+            _documento = new Document();
+            _documento.SetPageSize(PageSize.A4.Rotate());
+            var escritor = PdfWriter.GetInstance(_documento, new FileStream(Path.Combine(caminho, nomeDoArquivoDeSaida), FileMode.Create));
+            escritor.PageEvent = new Ouvinte(_Fonte1, _Fonte2, _Fonte3, _Fonte4, empresa);
+            escritor.AddViewerPreference(PdfName.PRINTSCALING, PdfName.NONE);
+            escritor.AddViewerPreference(PdfName.PICKTRAYBYPDFSIZE, PdfName.NONE);
+            return nomeDoArquivoDeSaida;
+        }
+
+        private void MonteDocumentoDoRelatorio(OrdenacaoRelatorioGeralPatente ordenacaoRelatorio)
+        {
+            var corBackgroudHeader = new Color(211, 211, 211);
+            var tabela = new PdfPTable(1);
+
+            tabela.WidthPercentage = 100;
+
+            _processosDePatentes = ordenacaoRelatorio.Equals(OrdenacaoRelatorioGeralPatente.Cliente) ? _processosDePatentes.OrderBy(processo => processo.Patente.Clientes[0].Pessoa.Nome).ToList() : 
+                _processosDePatentes.OrderBy(processo => processo.Processo).ToList();
+
+            foreach (IProcessoDePatente processo in _processosDePatentes)
+            {
+                
+            }
+
+            _documento.Add(tabela);
+        }
+
+        private class Ouvinte : IPdfPageEvent
+        {
+            private Font font1;
+            private Font font2;
+            private Font font3;
+            private Font font4;
+            private IEmpresa empresa;
+
+            public Ouvinte(Font font1, Font font2, Font font3, Font font4, IEmpresa empresa)
+            {
+                this.font1 = font1;
+                this.font2 = font2;
+                this.font3 = font3;
+                this.font4 = font4;
+                this.empresa = empresa;
+            }
+
+            public void OnOpenDocument(PdfWriter writer, Document document) { }
+
+            public void OnStartPage(PdfWriter writer, Document document)
+            {
+                var pessoaJuridica = empresa.Pessoa as IPessoaJuridica;
+                Chunk imagem;
+
+                if (!string.IsNullOrEmpty(pessoaJuridica.Logomarca))
+                {
+                    var imghead = iTextSharp.text.Image.GetInstance(HttpContext.Current.Server.MapPath(pessoaJuridica.Logomarca));
+                    imagem = new Chunk(imghead, 0, 0);
+                }
+                else
+                    imagem = new Chunk("");
+
+                var dadosEmpresa = new Phrase();
+                dadosEmpresa.Add(pessoaJuridica.NomeFantasia + Environment.NewLine);
+
+                if (pessoaJuridica.Enderecos.Count > 0)
+                {
+                    var endereco = pessoaJuridica.Enderecos[0];
+                    dadosEmpresa.Add(endereco.ToString());
+                }
+
+                if (pessoaJuridica.Telefones.Count > 0)
+                {
+                    var telefone = pessoaJuridica.Telefones[0];
+                    dadosEmpresa.Add("Telefone " + telefone);
+                }
+
+                var tabelaHeader = new Table(2);
+                tabelaHeader.Border = 0;
+                tabelaHeader.Width = 100;
+
+                var cell = new Cell(new Phrase(imagem));
+                cell.Border = 0;
+                cell.Width = 30;
+                tabelaHeader.AddCell(cell);
+
+                var cell1 = new Cell(dadosEmpresa);
+                cell1.Border = 0;
+                cell1.Width = 70;
+                tabelaHeader.AddCell(cell1);
+
+                var linhaVazia = new Cell(new Phrase("\n", font2));
+                linhaVazia.Colspan = 2;
+                linhaVazia.DisableBorderSide(1);
+                tabelaHeader.AddCell(linhaVazia);
+                document.Add(tabelaHeader);
+
+                //Adicionando linha que informa o número da revista 
+                var tabelaTitulo = new Table(1);
+                tabelaTitulo.Border = 0;
+                tabelaTitulo.Width = 100;
+
+                var celulaTitulo = new Cell(new Phrase("Relatório Geral de Patentes " + DateTime.Now.ToString("dd/MM/yyyy") , font3));
+                celulaTitulo.Border = 0;
+                celulaTitulo.Width = 70;
+                celulaTitulo.Colspan = 1;
+
+                tabelaTitulo.AddCell(celulaTitulo);
+
+                var linhaVaziaNumeroRevista = new Cell(new Phrase("\n", font2));
+                linhaVaziaNumeroRevista.Border = 0;
+                linhaVaziaNumeroRevista.Width = 70;
+                linhaVaziaNumeroRevista.Colspan = 1;
+                linhaVaziaNumeroRevista.DisableBorderSide(1);
+
+                tabelaTitulo.AddCell(linhaVaziaNumeroRevista);
+                document.Add(tabelaTitulo);
+            }
+
+            public void OnEndPage(PdfWriter writer, Document document)
+            {
+                HeaderFooter rodape;
+                var texto = new StringBuilder();
+
+                texto.AppendLine(String.Concat("Impressão em: ", DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss")));
+                rodape = new HeaderFooter(new Phrase(texto.ToString() + " página :" + (document.PageNumber + 1), font4), false);
+                rodape.Border = HeaderFooter.NO_BORDER;
+                rodape.Alignment = HeaderFooter.ALIGN_RIGHT;
+                document.Footer = rodape;
+            }
+
+            public void OnCloseDocument(PdfWriter writer, Document document) { }
+            public void OnParagraph(PdfWriter writer, Document document, float paragraphPosition) { }
+            public void OnParagraphEnd(PdfWriter writer, Document document, float paragraphPosition) { }
+            public void OnChapter(PdfWriter writer, Document document, float paragraphPosition, Paragraph title) { }
+            public void OnChapterEnd(PdfWriter writer, Document document, float paragraphPosition) { }
+            public void OnSection(PdfWriter writer, Document document, float paragraphPosition, int depth, Paragraph title) { }
+            public void OnSectionEnd(PdfWriter writer, Document document, float paragraphPosition) { }
+            public void OnGenericTag(PdfWriter writer, Document document, Rectangle rect, string text) { }
+        }
     }
 }
