9f4379bc8262e7c8b80f11ad6d6a6bf97cd4c724
hermessilvajunior <hermessilvajunior@gmail.com>
2014-04-30 20:09:34 +0000

    Criando novos metodos para envio de email

--- src/Compartilhados.Interfaces/GerenciadorDeEmail.vb
+++ src/Compartilhados.Interfaces/GerenciadorDeEmail.vb
@@ -13,7 +13,7 @@ Public Class GerenciadorDeEmail
                                  ByVal DestinatariosEmCopiaOculta As IList(Of String), _
                                  ByVal Mensagem As String,
                                  ByVal Anexos As IDictionary(Of String, Stream), _
-                                 ByVal Conexto As String, _
+                                 ByVal Contexto As String, _
                                  ByVal GravaHistorico As Boolean)
         Dim Configuracao As IConfiguracaoDoSistema
 
@@ -21,53 +21,9 @@ Public Class GerenciadorDeEmail
             Configuracao = ServicoDeConfiguracao.ObtenhaConfiguracaoDoSistema()
         End Using
 
-        If Not Configuracao Is Nothing AndAlso Not Configuracao.ConfiguracaoDeEmailDoSistema Is Nothing Then
-            Dim Gerenciador As SmtpClient
-
-            With Configuracao.ConfiguracaoDeEmailDoSistema
-                Gerenciador = New SmtpClient(.ServidorDeSaidaDeEmail, .Porta)
-
-                If .RequerAutenticacao Then
-                    Gerenciador.Credentials = New NetworkCredential(.UsuarioDeAutenticacaoDoServidorDeSaida, _
-                                                                    AjudanteDeCriptografia.Descriptografe(.SenhaDoUsuarioDeAutenticacaoDoServidorDeSaida))
-                End If
-
-                Gerenciador.EnableSsl = .HabilitarSSL
-
-                Dim MensagemDeEmail As MailMessage = New MailMessage
-                MensagemDeEmail.From = New MailAddress(Remetente)
-
-                For Each Destinatario As String In DestinatariosEmCopia
-                    MensagemDeEmail.CC.Add(New MailAddress(Destinatario))
-                Next
-
-                If Not DestinatariosEmCopiaOculta Is Nothing AndAlso DestinatariosEmCopiaOculta.Count > 0 Then
-                    For Each DestinatarioOculto As String In DestinatariosEmCopiaOculta
-                        MensagemDeEmail.Bcc.Add(New MailAddress(DestinatarioOculto))
-                    Next
-                End If
-
-                MensagemDeEmail.IsBodyHtml = True
-                MensagemDeEmail.Subject = Assunto
-                MensagemDeEmail.Body = Mensagem
-
-                If Not Anexos Is Nothing AndAlso Anexos.Count > 0 Then
-                    For Each item As KeyValuePair(Of String, Stream) In Anexos
-                        Dim anexo = New Attachment(item.Value, item.Key)
-
-                        MensagemDeEmail.Attachments.Add(anexo)
-                    Next
-                End If
-
-                Try
-                    Gerenciador.Send(MensagemDeEmail)
-                Catch ex As Exception
-                    Logger.GetInstancia().Erro("Ocorreu um erro ao tentar enviar um e-mail", ex)
-                    Throw
-                End Try
-
-            End With
-        End If
+        Using Servico As IServicoDeEnvioDeEmail = FabricaGenerica.GetInstancia.CrieObjeto(Of IServicoDeEnvioDeEmail)()
+            Servico.EnviaEmail(Configuracao, Assunto, Remetente, DestinatariosEmCopia, DestinatariosEmCopiaOculta, Mensagem, Anexos, Contexto, GravaHistorico)
+        End Using
     End Sub
 
 End Class
---
+++ src/Core.Interfaces/Mapeadores/IMapeadorDeHistoricoDeEmail.vb
@@ -0,0 +1,15 @@
+﻿Imports Core.Interfaces.Negocio
+Imports System.IO
+
+Namespace Mapeadores
+
+    Public Interface IMapeadorDeHistoricoDeEmail
+
+        Sub Grave(Historio As IHistoricoDeEmail)
+        Sub Exclua(IdHistorico As Long)
+        Function ObtenhaAnexos(IdHistorico as long) As IDictionary(Of String, Stream)
+
+    End Interface
+
+
+End Namespace
