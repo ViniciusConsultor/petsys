31ea4521e8641e2a8abf6ae9e2469e7a76ba8f9b
hermessilvajunior <hermessilvajunior@gmail.com>
2014-05-22 18:04:15 +0000

    Implementação parcial do HistoricoDeEmail

--- src/Core.Interfaces/Mapeadores/IMapeadorDeHistoricoDeEmail.vb
+++ src/Core.Interfaces/Mapeadores/IMapeadorDeHistoricoDeEmail.vb
@@ -11,6 +11,7 @@ Namespace Mapeadores
         Function ObtenhaAnexos(IdHistorico As Long) As IDictionary(Of String, StreamReader)
         Function ObtenhaHistoricos(Filtro As IFiltro, QuantidadeDeItens As Integer, OffSet As Integer) As IList(Of IHistoricoDeEmail)
         Function ObtenhaQuantidadeDeHistoricoDeEmails(Filtro As IFiltro) As Integer
+        Function ObtenhaHistorico(IDHistorico As Long) As IHistoricoDeEmail
     End Interface
 
 
