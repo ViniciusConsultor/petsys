513792d5c36e06be4f925123d9cf33651eb17443
mrmarins89 <mrmarins89@gmail.com>
2014-05-23 01:37:21 +0000

    Reversão da propriedade adicionada no objeto cliente

--- src/Compartilhados.Interfaces/Core/Negocio/ICliente.vb
+++ src/Compartilhados.Interfaces/Core/Negocio/ICliente.vb
@@ -13,7 +13,6 @@
         Property PorcentagemDeDescontoAutomatico() As Nullable(Of Double)
         Property ValorMaximoParaCompras() As Nullable(Of Double)
         Property SaldoParaCompras() As Nullable(Of Double)
-        Property PossuiCobranca As Boolean
 
     End Interface
 
