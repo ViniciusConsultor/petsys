0a72d77dbb4c3961573094410f4bf97da36910e8
mrmarins89 <mrmarins89@gmail.com>
2014-05-27 02:51:56 +0000

    Implementação parcial do relatório de manutenções

---
+++ src/FN.Client/FN/Relatorios/GeradorDeRelatorioDeManutencoes.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FN.Client.FN.Relatorios
+{
+    public class GeradorDeRelatorioDeManutencoes
+    {
+    }
+}
