faad5bacee76729dcfbfb637c437b30767595a1d
hermessilvajunior@gmail.com <hermessilvajunior@gmail.com@d652af1f-c5ef-232d-d197-2330bd19a0a3>
2015-02-19 14:46:10 +0000

    Criação de nova coluna na grid de processos e marcas e patentes (status) Criação da nova funcionalidade que traz as marcas e patentes de determinado cliente.

--- branches/2014/AjustesWagner/src/MP.Client/MP/DTOProcessoDeMarca.cs
+++ branches/2014/AjustesWagner/src/MP.Client/MP/DTOProcessoDeMarca.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Compartilhados.Interfaces.Core.Negocio;
+using MP.Interfaces.Negocio;
 
 namespace MP.Client.MP
 {
@@ -20,6 +22,62 @@ namespace MP.Client.MP
         public string Cliente { get; set; }
         public string DataDoCadastro { get; set; }
         public string IdCliente { get; set; }
+        public string Status { get; set; }
+
+
+        public static IList<DTOProcessoDeMarca> ConvertaProcessoParaDTO(IList<IProcessoDeMarca> processosDeMarca)
+        {
+            var processos = new List<DTOProcessoDeMarca>();
+
+            foreach (var processo in processosDeMarca)
+            {
+                var dto = new DTOProcessoDeMarca();
+
+                if (processo.Marca.Apresentacao != null)
+                    dto.Apresentacao = processo.Marca.Apresentacao.Nome;
+
+                if (processo.Marca.Cliente.Pessoa.Tipo == TipoDePessoa.Fisica)
+                {
+                    var cpf = processo.Marca.Cliente.Pessoa.ObtenhaDocumento(TipoDeDocumento.CPF);
+
+                    if (cpf != null)
+                        dto.CPFCNPJ = cpf.ToString();
+
+                }
+                else
+                {
+                    var cnpj = processo.Marca.Cliente.Pessoa.ObtenhaDocumento(TipoDeDocumento.CNPJ);
+
+                    if (cnpj != null)
+                        dto.CPFCNPJ = cnpj.ToString();
+                }
+
+                if (processo.Marca.NCL != null)
+                    dto.Classe = processo.Marca.NCL.Codigo;
+
+                dto.Cliente = processo.Marca.Cliente.Pessoa.Nome;
+                dto.IdCliente = processo.Marca.Cliente.Pessoa.ID.Value.ToString();
+
+                if (processo.DataDoDeposito != null)
+                    dto.DataDeposito = processo.DataDoDeposito.Value.ToString("dd/MM/yyyy");
+
+                dto.DataDoCadastro = processo.DataDoCadastro.ToString("dd/MM/yyyy");
+                dto.DescricaoMarca = processo.Marca.DescricaoDaMarca;
+
+                dto.IdProcesso = processo.IdProcessoDeMarca.Value.ToString();
+
+                dto.Natureza = processo.Marca.Natureza.Nome;
+
+                dto.NumeroProcesso = processo.Processo.ToString();
+
+                dto.Status = processo.Ativo ? "Ativo" : "Inativo";
+
+                processos.Add(dto);
+
+            }
+
+            return processos;
+        }
 
     }
 }
---
+++ branches/2014/AjustesWagner/src/MP.Client/MP/DTOProcessoDePatente.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MP.Interfaces.Negocio;
+
+namespace MP.Client.MP
+{
+    public class DTOProcessoDePatente
+    {
+
+        public string IdProcessoDePatente { get; set; }
+        public string NumeroDoProcessoFormatado { get; set; }
+        public string SiglaNatureza { get; set; }
+        public string TituloPatente { get; set; }
+        public string DataDoDeposito { get; set; }
+        public string Status { get; set; }                                
+                                            
+        public static IList<DTOProcessoDePatente> ConvertaProcessoParaDTO(IList<IProcessoDePatente> processosDePatente)
+        {
+            var dtos = new List<DTOProcessoDePatente>();
+
+            foreach (var processo in processosDePatente)
+            {
+                var dto = new DTOProcessoDePatente();
+
+                dto.IdProcessoDePatente = processo.IdProcessoDePatente.Value.ToString();
+                dto.NumeroDoProcessoFormatado = processo.NumeroDoProcessoFormatado;
+                dto.SiglaNatureza = processo.Patente.NaturezaPatente.SiglaNatureza;
+                dto.TituloPatente = processo.Patente.TituloPatente;
+
+                if (processo.DataDoDeposito.HasValue)
+                    dto.DataDoDeposito = processo.DataDoDeposito.Value.ToString("dd/MM/yyyy");
+                dto.Status = processo.Ativo ? "Ativo" : "Inativo";
+
+                dtos.Add(dto);
+            }
+
+            return dtos;
+        }
+                  
+    }
+}
