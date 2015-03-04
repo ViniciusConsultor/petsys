ce3afdb4fffddb0c290d93beb809321b785c3359
hermessilvajunior@gmail.com <hermessilvajunior@gmail.com@d652af1f-c5ef-232d-d197-2330bd19a0a3>
2015-03-04 11:28:14 +0000

    Criando o novo modulo de pesquisa de marcas e patentes

---
+++ branches/2014/AjustesWagner/src/PMP.Client/PMP/frmProcessarEmLote.aspx
@@ -0,0 +1,6 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/WorkSpace.Master" AutoEventWireup="true" CodeBehind="frmProcessarEmLote.aspx.cs" Inherits="PMP.Client.PMP.frmProcessarEmLote" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+    <asp:Button ID="btnProcessar" runat="server" Text="Processar" OnClick=btnProcessar_OnClick />
+</asp:Content>
---
+++ branches/2014/AjustesWagner/src/PMP.Client/PMP/frmProcessarEmLote.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Compartilhados.Fabricas;
+using PMP.Interfaces.Servicos;
+
+namespace PMP.Client.PMP
+{
+    public partial class frmProcessarEmLote : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnProcessar_OnClick(object sender, EventArgs e)
+        {
+           using (var servico = FabricaGenerica.GetInstancia().CrieObjeto<IServicoDeProcessoDeMarcaDeRevista>())
+           {
+               servico.ProcesseEmLote(TextBox1.Text);
+           }
+        }
+    }
+}
