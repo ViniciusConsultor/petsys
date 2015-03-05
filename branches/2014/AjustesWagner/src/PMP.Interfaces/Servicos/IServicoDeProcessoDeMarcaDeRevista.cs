0df49fd6854c9a48dc7d3d29fa9c88ae4a4dc28e
hermessilvajunior@gmail.com <hermessilvajunior@gmail.com@d652af1f-c5ef-232d-d197-2330bd19a0a3>
2015-03-05 21:01:58 +0000

    Implementando filtro por processo na consulta por marca

--- branches/2014/AjustesWagner/src/PMP.Interfaces/Servicos/IServicoDeProcessoDeMarcaDeRevista.cs
+++ branches/2014/AjustesWagner/src/PMP.Interfaces/Servicos/IServicoDeProcessoDeMarcaDeRevista.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Compartilhados;
+using Compartilhados.Interfaces.Core.Negocio;
+using PMP.Interfaces.Utilidades;
 
 namespace PMP.Interfaces.Servicos
 {
     public interface IServicoDeProcessoDeMarcaDeRevista : IServico
     {
         void ProcesseEmLote(string pastaDeArmazenamentoDasRevistas);
+        IList<DTOProcessoMarcaRevista> ObtenhaResultadoDaPesquisa(IFiltro filtro, int quantidadeDeRegistros, int offSet);
     }
 }
---
+++ branches/2014/AjustesWagner/src/PMP.Negocio/Filtros/Marca/FiltroPorNumeroDeProcesso.cs
@@ -0,0 +1,43 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="FiltroPorNumeroDeProcesso.cs" company="Microsoft">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using Core.Negocio;
+using PMP.Interfaces.Negocio.Filtros.Marca;
+
+namespace PMP.Negocio.Filtros.Marca
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    /// <summary>
+    /// TODO: Update summary.
+    /// </summary>
+    [Serializable]
+    public class FiltroPorNumeroDeProcesso : Filtro, IFiltroPorNumeroDeProcesso
+    {
+        public override string ObtenhaQuery()
+        {
+            var sql = new StringBuilder();
+
+            sql.Append("SELECT NUMERODAREVISTA, DATAPUBLICACAOREVISTA, NUMEROPROCESSODEMARCA, DATADODEPOSITO, ");
+            sql.Append("DATADACONCESSAO, DATADAVIGENCIA, CODIGODODESPACHO, NOMEDODESPACHO, ");
+            sql.Append("TEXTOCOMPLEMENTARDESPACHO, TITULAR, PAISTITULAR, UFTITULAR, ");
+            sql.Append("MARCA, APRESENTACAO, NATUREZA, EDICAOCLASSEVIENA, ");
+            sql.Append("CODIGOCLASSEVIENA, CODIGOCLASSENACIONAL, CODIGOSUBCLASSENACIONAL, CODIGOCLASSENICE, ");
+            sql.Append("ESPECIFICACAOCLASSENICE, PROCURADOR, APOSTILA FROM PMP_PROCESSOSMARCAREVISTA WHERE ");
+            sql.Append(ObtenhaFiltroMontado("NUMEROPROCESSODEMARCA", false));
+
+            if (NumeroDaRevista != null)
+                sql.Append(" AND NUMERODAREVISTA = " + NumeroDaRevista.Value);
+
+            return sql.ToString();
+        }
+
+        public int? NumeroDaRevista { get; set; }
+    }
+}
