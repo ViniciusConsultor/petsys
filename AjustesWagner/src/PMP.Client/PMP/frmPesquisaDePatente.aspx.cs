7260a24074b53aada1cde5959e95b7cfbe80bf50
hermessilvajunior <hermessilvajunior@gmail.com>
2015-03-20 16:57:54 +0000

    Iniciando o desenvolvimento de pesquisa por patente

---
+++ AjustesWagner/src/PMP.Client/PMP/frmPesquisaDePatente.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Compartilhados.Componentes.Web;
+using Telerik.Web.UI;
+
+namespace PMP.Client.PMP
+{
+    public partial class frmPesquisaDePatente :  SuperPagina
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected override string ObtenhaIdFuncao()
+        {
+            return "FUN.PMP.003";
+        }
+
+        protected override RadToolBar ObtenhaBarraDeFerramentas()
+        {
+            return null;
+        }
+    }
+}
---
+++ AjustesWagner/src/PMP.Servicos.Local/ServicoDeProcessoDePatenteDeRevistaLocal.cs
@@ -0,0 +1,67 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="ServicoDeProcessoDePatenteDeRevistaLocal.cs" company="Microsoft">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using Compartilhados;
+using Compartilhados.Fabricas;
+using Compartilhados.Interfaces.Core.Negocio;
+using PMP.Interfaces.Mapeadores;
+using PMP.Interfaces.Servicos;
+using PMP.Interfaces.Utilidades;
+
+namespace PMP.Servicos.Local
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    /// <summary>
+    /// TODO: Update summary.
+    /// </summary>
+    public class ServicoDeProcessoDePatenteDeRevistaLocal : Servico, IServicoDeProcessoDePatenteDeRevista
+    {
+        public ServicoDeProcessoDePatenteDeRevistaLocal(ICredencial Credencial) : base(Credencial)
+        {
+        }
+
+        public IList<string> ProcesseEmLote(string pastaDeArmazenamentoDasRevistas)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<DTOProcessoPatenteRevista> ObtenhaResultadoDaPesquisa(IFiltro filtro, int quantidadeDeRegistros, int offSet)
+        {
+            ServerUtils.setCredencial(_Credencial);
+
+            var mapeador = FabricaGenerica.GetInstancia().CrieObjeto<IMapeadorDeProcessoDePatenteRevista>();
+
+            try
+            {
+                return mapeador.ObtenhaResultadoDaPesquisa(filtro, quantidadeDeRegistros, offSet);
+            }
+            finally
+            {
+                ServerUtils.libereRecursos();
+            } 
+        }
+
+        public int ObtenhaQuantidadeDeResultadoDaPesquisa(IFiltro filtro)
+        {
+            ServerUtils.setCredencial(_Credencial);
+
+            var mapeador = FabricaGenerica.GetInstancia().CrieObjeto<IMapeadorDeProcessoDePatenteRevista>();
+
+            try
+            {
+                return mapeador.ObtenhaQuantidadeDeResultadoDaPesquisa(filtro);
+            }
+            finally
+            {
+                ServerUtils.libereRecursos();
+            } 
+        }
+    }
+}
