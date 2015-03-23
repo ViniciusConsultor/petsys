2ea434fb4fef577a84127c4b986cd285484688be
hermessilvajunior <hermessilvajunior@gmail.com>
2015-03-23 20:50:00 +0000

    Iniciando o desenvolvimento de pesquisa por patente

--- AjustesWagner/src/PMP.Interfaces/Utilidades/DTOProcessoPatenteRevista.cs
+++ AjustesWagner/src/PMP.Interfaces/Utilidades/DTOProcessoPatenteRevista.cs
@@ -22,15 +22,24 @@ namespace PMP.Interfaces.Utilidades
         public DateTime DataDePublicacaoDaRevista { get; set; }
         public string NumeroProcesso { get; set; }
         public DateTime? DataDoDeposito { get; set; }
+        public DateTime? DataDaConcessao { get; set; }
         public string Classificacao { get; set; }
         public string Titulo { get; set; }
         public string Resumo { get; set; }
         public string NumeroDepositoPCT { get; set; }
-        public IList<string>  Depositantes { get; set; }
-        public IList<string> Invetores { get; set; }
+        public string Depositante { get; set; }
+        public string PaisDepositante { get; set; }
+        public string UFDepositante { get; set; }
+        public IList<string> Inventores { get; set; }
         public string NumeroDaPrioridade { get; set; }
         public DateTime? DataDaPrioridade { get; set; }
         public string PaisDaPrioridade { get; set; }
+        public string Procurador { get; set; }
+        public string Despacho { get; set; }
+        public string Titular { get; set; }
+        public string PaisTitular { get; set; }
+        public string UFTitular { get; set; }
+        public string NumeroDoPedido { get; set; }
 
     }
 }
--- AjustesWagner/src/PMP.Servicos.Local/ServicoDeProcessoDePatenteDeRevistaLocal.cs
+++ AjustesWagner/src/PMP.Servicos.Local/ServicoDeProcessoDePatenteDeRevistaLocal.cs
@@ -4,9 +4,12 @@
 // </copyright>
 // -----------------------------------------------------------------------
 
+using System.IO;
+using System.Xml.Linq;
 using Compartilhados;
 using Compartilhados.Fabricas;
 using Compartilhados.Interfaces.Core.Negocio;
+using MP.Interfaces.Utilidades;
 using PMP.Interfaces.Mapeadores;
 using PMP.Interfaces.Servicos;
 using PMP.Interfaces.Utilidades;
@@ -27,9 +30,87 @@ namespace PMP.Servicos.Local
         {
         }
 
+        private void DescompacteArquivos(string pastaDeArmazenamentoDasRevistas, string pastaDeDestino)
+        {
+            var informacoesDoDiretorio = new DirectoryInfo(pastaDeArmazenamentoDasRevistas);
+            var arquivosCompactados = informacoesDoDiretorio.GetFiles();
+
+            foreach (var arquivoCompactado in arquivosCompactados)
+                Util.DescompacteArquivoZip(arquivoCompactado.FullName, pastaDeDestino);
+
+        }
+
+        private string TransformeTXTParaXml(FileInfo arquivoTxt)
+        {
+            var numeroDaRevista = arquivoTxt.Name.Substring(2, 4);
+            var dataRevista = arquivoTxt.LastWriteTime;
+
+            using (var arquivo = new StreamReader(arquivoTxt.FullName))
+            {
+                TradutorDeRevistaPatenteTXTParaRevistaPatenteXML.TraduzaRevistaDePatente(dataRevista, numeroDaRevista, arquivo, arquivoTxt.Directory.FullName + "\\");
+                arquivo.Close();
+            }
+
+            return Path.Combine(arquivoTxt.Directory.FullName, numeroDaRevista + ".xml");
+        }
+
+        private KeyValuePair<int, IList<DTOProcessoPatenteRevista>> ExtraiaProcessoDePatenteDeRevistaDoArquivo(FileInfo arquivo)
+        {
+            //Verificar se arquivo é um txt, se for converter primeiro
+            var arquivoDaRevista = XDocument.Load(arquivo.Extension.ToUpper().Equals(".TXT") ? TransformeTXTParaXml(arquivo) : arquivo.FullName);
+            var conteudoDaRevista = arquivoDaRevista.Element("revista");
+            var numeroDaRevista = int.Parse(conteudoDaRevista.Attribute("numero").Value);
+            var dataDaPublicacaoDaRevista = DateTime.Parse(conteudoDaRevista.Attribute("data").Value);
+
+            var listaDeProcessosNaRevista = (from conteudoProcesso in conteudoDaRevista.Elements("processo")
+                                             select new DTOProcessoPatenteRevista()
+                                             {
+                                                 ID = Guid.NewGuid().ToString(),
+                                                 NumeroDaRevista = numeroDaRevista,
+                                                 DataDePublicacaoDaRevista = dataDaPublicacaoDaRevista,
+                                                 NumeroProcesso = conteudoProcesso.Element("patente") == null ? null : conteudoProcesso.Element("patente").Attribute("numero").Value,
+                                                 Procurador = conteudoProcesso.Element("procurador") == null ? null : UtilidadesDeString.RemoveAcentos(conteudoProcesso.Element("procurador").Value),
+                                                 DataDoDeposito = conteudoProcesso.Attribute("data-deposito") == null ? (DateTime?)null : DateTime.Parse(conteudoProcesso.Attribute("data-deposito").Value),
+                                                 DataDaConcessao = conteudoProcesso.Attribute("data-concenssao") == null ? (DateTime?)null : DateTime.Parse(conteudoProcesso.Attribute("data-concenssao").Value),
+                                                 Despacho = conteudoProcesso.Element("despachos").Element("despacho").Attribute("codigo").Value,
+                                                 Titular = conteudoProcesso.Element("titulares") == null ? null : conteudoProcesso.Element("titulares").Element("titular") == null ? null : conteudoProcesso.Element("titulares").Element("titular").Attribute("nome-razao-social") == null ? null : UtilidadesDeString.RemoveAcentos(conteudoProcesso.Element("titulares").Element("titular").Attribute("nome-razao-social").Value),
+                                                 UFTitular = conteudoProcesso.Element("titulares") == null ? null : conteudoProcesso.Element("titulares").Element("titular") == null ? null : conteudoProcesso.Element("titulares").Element("titular").Attribute("uf") == null ? null : conteudoProcesso.Element("titulares").Element("titular").Attribute("uf").Value,
+                                                 PaisTitular = conteudoProcesso.Element("titulares") == null ? null : conteudoProcesso.Element("titulares").Element("titular") == null ? null : conteudoProcesso.Element("titulares").Element("titular").Attribute("pais") == null ? null : conteudoProcesso.Element("titulares").Element("titular").Attribute("pais").Value,
+                                                 Titulo = conteudoProcesso.Element("patente") == null ? null : conteudoProcesso.Element("patente").Element("titulo") == null ?  null :  UtilidadesDeString.RemoveAcentos(conteudoProcesso.Element("patente").Element("titulo").Value),
+                                                 Inventores = conteudoProcesso.Element("patente") == null ? null : conteudoProcesso.Element("patente").Element("inventores") == null ? null :  UtilidadesDePersistencia.MapeieStringParaListaDeString(UtilidadesDeString.RemoveAcentos(conteudoProcesso.Element("patente").Element("inventores").Value),','),
+                                                 Depositante = conteudoProcesso.Element("depositante") == null ? null : UtilidadesDeString.RemoveAcentos(conteudoProcesso.Element("depositante").Value),
+                                                 UFDepositante = conteudoProcesso.Element("depositante") == null ? null : conteudoProcesso.Element("depositante").Attribute("uf") == null ? null : conteudoProcesso.Element("depositante").Attribute("uf").Value,
+                                                 PaisDepositante = conteudoProcesso.Element("depositante") == null ? null : conteudoProcesso.Element("depositante").Attribute("pais") == null ? null : conteudoProcesso.Element("depositante").Attribute("pais").Value,
+                                                 NumeroDoPedido = conteudoProcesso.Element("numeroDoPedido") == null ? null : conteudoProcesso.Element("numeroDoPedido").Value,
+                                                 
+                                             }).ToList();
+
+            return new KeyValuePair<int, IList<DTOProcessoPatenteRevista>>(numeroDaRevista, listaDeProcessosNaRevista);
+        }
+
         public IList<string> ProcesseEmLote(string pastaDeArmazenamentoDasRevistas)
         {
-            throw new NotImplementedException();
+            if (!Directory.Exists(pastaDeArmazenamentoDasRevistas))
+                throw new ApplicationException("Pasta informada não existe!");
+
+            var pastaDeDestinoDaDescompactacao = Path.Combine(pastaDeArmazenamentoDasRevistas, "ArquivosDescompactados");
+
+            DescompacteArquivos(pastaDeArmazenamentoDasRevistas, pastaDeDestinoDaDescompactacao);
+
+            var informacoesDoDiretorio = new DirectoryInfo(pastaDeDestinoDaDescompactacao);
+            var arquivosDesompactados = informacoesDoDiretorio.GetFiles();
+
+            IDictionary<int, IList<DTOProcessoPatenteRevista>> listaDeProcessosDePatenteDeRevista = new Dictionary<int, IList<DTOProcessoPatenteRevista>>();
+
+            foreach (var arquivo in arquivosDesompactados)
+                listaDeProcessosDePatenteDeRevista.Add(ExtraiaProcessoDePatenteDeRevistaDoArquivo(arquivo));
+
+            return GraveEmLote(listaDeProcessosDePatenteDeRevista);
+        }
+
+        private IList<string> GraveEmLote(IDictionary<int, IList<DTOProcessoPatenteRevista>> listaDeProcessosDePatenteDeRevista)
+        {
+            return null;
         }
 
         public IList<DTOProcessoPatenteRevista> ObtenhaResultadoDaPesquisa(IFiltro filtro, int quantidadeDeRegistros, int offSet)
